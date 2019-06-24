fc4c4e34934fd4a953e63a8b48ed6f16cc0efde6
Speare, Benjamin C <benjamin-speare@uiowa.edu>
2019-06-24 13:34:50 -0500

    Removed container class for base elements in preparation for #1

--- docs/components/raw/feature-block/feature-block.js
+++ docs/components/raw/feature-block/feature-block.js
@@ -1,7 +1,7 @@
 let feature = document.getElementById("feature__container");
 // moves over the div and remove class
 if (document.getElementById("feature__container")) {
-feature.addEventListener("mouseenter", function (event) {
-  document.getElementsByClassName('feature')[0].classList.remove("is-open");
-});
+  feature.addEventListener("mouseenter", function (event) {
+    document.getElementsByClassName('feature')[0].classList.remove("is-open");
+  });
 }
