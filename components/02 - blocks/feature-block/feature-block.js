5e3613b8a59a851445621e5d259baab0bc76d366
Benjamin Speare <benjamin-speare@uiowa.edu>
2019-08-16 12:59:04 -0500

    Added background video option in feature block component #8

--- components/02 - blocks/feature-block/feature-block.js
+++ components/02 - blocks/feature-block/feature-block.js
@@ -14,3 +14,23 @@ for (let i = 0; i < elements.length; i++) {
     this.setAttribute("aria-expanded", "true");
   };
 }
+
+
+document.getElementById("video-btn").addEventListener("click", pausePlay);
+const video = document.getElementById("video-container");
+const btn = document.getElementById("video-btn");
+
+// Pause and play
+function pausePlay() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "<span class='element-invisible'>" + "Pause" + "</span>";
+    btn.classList.remove("video-btn__play");
+    btn.classList.add("video-btn__pause");
+  } else {
+    video.pause();
+    btn.innerHTML = "<span class='element-invisible'>" + "Play" + "</span>";
+    btn.classList.remove("video-btn__pause");
+    btn.classList.add("video-btn__play");
+  }
+}
