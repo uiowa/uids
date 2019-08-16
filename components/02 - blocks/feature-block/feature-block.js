edbd8e21d8f57ef10b2560d30b1c00ba7a1e0921
Benjamin Speare <benjamin-speare@uiowa.edu>
2019-08-16 09:58:56 -0500

    Added aria to feature block component #7

--- components/02 - blocks/feature-block/feature-block.js
+++ components/02 - blocks/feature-block/feature-block.js
@@ -1,9 +1,16 @@
-/*
-let feature = document.getElementById("feature__container");
-// moves over the div and remove class
-if (document.getElementById("feature__container")) {
-  feature.addEventListener("mouseenter", function (event) {
-    document.getElementsByClassName('feature')[0].classList.remove("is-open");
-  });
+const elements = document.getElementsByClassName("feature");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].onmouseover = elements[i].onfocus = function () {
+    // remove attribute from sibling
+    let el = elements[0];
+    while (el) {
+      if (el.tagName === "A") {
+        //remove attribute
+        el.setAttribute("aria-expanded", "false");
+      }
+      // pass the attribute to sibling
+      el = el.nextSibling;
+    }
+    this.setAttribute("aria-expanded", "true");
+  };
 }
-*/
