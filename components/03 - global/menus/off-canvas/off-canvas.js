5b58b57393b7521454d4dc8b331ce05c783c792d
Benjamin Speare <benjamin-speare@uiowa.edu>
2020-01-27 12:08:13 -0600

    Beginning steps to refactor structure of header

--- components/03 - global/menus/off-canvas/off-canvas.js
+++ components/03 - global/menus/off-canvas/off-canvas.js
@@ -88,7 +88,7 @@ document.addEventListener(
 
 // esc key
 
-window.addEventListener("keydown", event => {
+window.addEventListener("keydown", function (event) {
   if (event.key === "Escape") {
     document.body.classList.remove("o-canvas--lock");
     document.body.classList.add("o-canvas--complete");
@@ -99,4 +99,34 @@ window.addEventListener("keydown", event => {
       toggleButtons[i].classList.remove("active");
     }
   }
+});
+
+
+
+
+//const body = document.body;
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+const canvasLock = "o-canvas--lock";
+let lastScroll = 0;
+
+
+
+window.addEventListener("scroll", function () {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+// if contains lock don't add anything....
+  if (currentScroll > lastScroll && !body.classList.contains(canvasLock)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
 });
