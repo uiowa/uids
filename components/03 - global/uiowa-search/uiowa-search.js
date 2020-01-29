f54115711739a4ac5bb4fced3776dddfd575fea5
Benjamin Speare <benjamin-speare@uiowa.edu>
2020-01-29 14:36:38 -0600

    Additional refactoring and removing of header components

--- components/03 - global/uiowa-search/uiowa-search.js
+++ components/03 - global/uiowa-search/uiowa-search.js
@@ -2,7 +2,6 @@
   const body = document.body;
   const button = document.querySelector("button.search-button");
 
-
   if (document.querySelector(".search-button")) {
   document.querySelector(".search-button").addEventListener("click", searchToggle);
   document.querySelector(".search-button").addEventListener('keydown', function (event) {
@@ -14,31 +13,21 @@
 }
 
 function searchToggle() {
-
-// document.getElementById("#bttnav").removeEventListener("click");
-
   if (body.classList.contains("search-is-open")) {
     this.setAttribute("aria-expanded", "false");
-    //wrapper.classList.remove("is-open");
     wrapper.setAttribute("aria-expanded", "false");
     body.classList.remove("search-is-open");
   } else {
-    //wrapper.classList.add("is-open");
     wrapper.setAttribute("aria-expanded", "true");
     this.setAttribute("aria-expanded", "true");
     body.classList.add("search-is-open");
   }
-
-    //const isAriaPressed = button.getAttribute('aria-pressed') === 'true';
-    //button.setAttribute('aria-pressed', isAriaPressed ? 'false' : 'true');
-
     if (button.getAttribute('aria-expanded') === 'true') {
       document.getElementById('search-button-label').innerHTML = 'Close';
       document.getElementById("search-input").focus();
     } else {
       document.getElementById('search-button-label').innerHTML = 'Search';
     }
-
 }
 
 
@@ -49,11 +38,9 @@ document.addEventListener(
   function (event) {
     if (!event.target.closest(".search-wrapper")) {
      document.body.classList.remove("search-is-open");
-      document.getElementById('search-button-label').innerHTML = 'Search';
-    // wrapper.classList.remove("is-open");
+     document.getElementById('search-button-label').innerHTML = 'Search';
      wrapper.setAttribute("aria-expanded", "false");
      button.setAttribute("aria-expanded", "false");
-        console.log("outside");
     }
   },
   false
