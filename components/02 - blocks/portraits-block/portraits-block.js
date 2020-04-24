9a89b8f341b80f395fde20d7eed51e1e15168f4b
Benjamin Speare <benjamin-speare@uiowa.edu>
2020-04-24 10:07:42 -0500

    Updated portrait block and so that video controls appear for mobile by default.

--- components/02 - blocks/portraits-block/portraits-block.js
+++ components/02 - blocks/portraits-block/portraits-block.js
@@ -3,22 +3,11 @@ const ctrlVideo = document.querySelectorAll(".player");
 const textOverlay = document.querySelectorAll(".portrait .highlight__wrapper");
 const videoContainer = document.querySelectorAll(".embed-responsive");
 const buttonLabel = document.getElementById("label");
+const debug = false;
 
-/*
-Object.defineProperty(HTMLMediaElement.prototype, "playing", {
-  get: function () {
-    return !!(
-      this.currentTime > 0 &&
-      !this.paused &&
-      !this.ended &&
-      this.readyState > 2
-    );
-  }
-});
-*/
 document.addEventListener(
-    "click",
-    function (e) {
+  "click",
+  function (e) {
     if (!e.target.classList.contains("vidbttn")) return;
     e.target.classList.toggle("paused");
     for (var i = 0; i < videoContainer.length; i++) {
@@ -30,13 +19,7 @@ document.addEventListener(
       e.target.classList.toggle("active");
     }
 
-    for (var i = 0; i < buttonLabel.length; i++) {
-    }
-    /*
-      if (document.querySelector('video').playing) {
-       console.log("play");
-      }
-*/
+    for (var i = 0; i < buttonLabel.length; i++) {}
 
     if (e.target.classList.contains("active")) {
       for (var i = 0; i < textOverlay.length; i++) {
@@ -45,6 +28,7 @@ document.addEventListener(
 
       for (var i = 0; i < ctrlVideo.length; i++) {
         document.getElementById("label").innerHTML = "Pause";
+        ctrlVideo[i].setAttribute("controls", "true");
       }
     } else {
       for (var i = 0; i < ctrlVideo.length; i++) {
@@ -61,48 +45,47 @@ document.addEventListener(
       if (toggleButton[i] === e.target) continue;
       ctrlVideo[i].pause();
       toggleButton[i].classList.remove("active");
-      //buttonLabel[i].innerHTML = "Play";
       textOverlay[i].classList.remove("active");
       toggleButton[i].classList.remove("paused");
       videoContainer[i].classList.remove("active");
+      ctrlVideo[i].removeAttribute("controls", "true");
     }
   },
   false
 );
 
+// Set video controls to true for small viewport
+// Define our viewportWidth variable
+var viewportWidth;
 
+// Set/update the viewportWidth value
+var setViewportWidth = function () {
+  viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+};
 
-const isMobile = {
-  Android() {
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBerry() {
-    return navigator.userAgent.match(/BlackBerry/i);
-  },
-  iOS() {
-    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-  },
-  iPhone() {
-    return navigator.userAgent.match(/iPhone/i);
-  },
-  iPad() {
-    return navigator.userAgent.match(/iPad/i);
-  },
-  Opera() {
-    return navigator.userAgent.match(/Opera Mini/i);
-  },
-  Windows() {
-    return navigator.userAgent.match(/IEMobile/i);
-  },
-  any() {
-    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+// Log the viewport width into the console
+var logWidth = function () {
+  if (viewportWidth > 768) {
+    if (debug) {
+      console.log("Wide viewport");
+    }
+  } else {
+    document.querySelectorAll(".player")[0].controls = true;
+    document.querySelectorAll(".player")[1].controls = true;
+    document.querySelectorAll(".player")[2].controls = true;
   }
 };
 
+// Set our initial width and log it
+setViewportWidth();
+logWidth();
 
-
-if (isMobile.any()) {
-  document.querySelectorAll(".player")[0].controls = true;
-  document.querySelectorAll(".player")[1].controls = true;
-  document.querySelectorAll(".player")[2].controls = true;
-}
+// On resize events, recalculate and log
+window.addEventListener(
+  "resize",
+  function () {
+    setViewportWidth();
+    logWidth();
+  },
+  false
+);
