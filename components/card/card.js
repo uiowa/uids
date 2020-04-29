1f1b92fb8215eb0050b052af6f7e609cc2ad5833
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-04-29 14:21:11 -0500

    Updated all templates to point to new 'card' component instead of 'card-block'. Added scss and js to card component. Updated references to css an js in preview template.

---
+++ components/card/card.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll('.card');
+Array.prototype.forEach.call(cards, card => {
+  if (card.querySelector('.card__title a')) {
+    var down, up, link = card.querySelector('.card__title a');
+  } else {
+    var down, up, link = card.querySelector('a.bttn');
+  }
+  if (card.querySelector('a.bttn') || card.querySelector('.card__title a')) {
+    card.style.cursor = 'pointer';
+    card.onmousedown = () => down = +new Date();
+    card.onmouseup = () => {
+      up = +new Date();
+      if ((up - down) < 200) {
+        link.click();
+      }
+    }
+  }
+});
