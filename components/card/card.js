3dd833f9f34d8c1ce5b295f147f51bff688d6ac0
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-05-18 20:39:47 -0500

    Updated card template to reflect changes in uids_base. Updated javascript to allow for using the image to attach the card link to.

--- components/card/card.js
+++ components/card/card.js
@@ -2,10 +2,12 @@ const cards = document.querySelectorAll('.card');
 Array.prototype.forEach.call(cards, card => {
   if (card.querySelector('.card__title a')) {
     var down, up, link = card.querySelector('.card__title a');
+  } else if (card.querySelector('.card__media a')) {
+    var down, up, link = card.querySelector('.card__media a');
   } else {
     var down, up, link = card.querySelector('a.bttn');
   }
-  if (card.querySelector('a.bttn') || card.querySelector('.card__title a')) {
+  if (card.querySelector('a.bttn') || card.querySelector('.card__title a') || card.querySelector('.card__media a')) {
     card.style.cursor = 'pointer';
     card.onmousedown = () => down = +new Date();
     card.onmouseup = () => {
