ec34ac3cf246fc82adfb4fcffec5507cf3f257cd
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-05-22 15:23:35 -0500

    Making a change so github will recognize that something is different.

--- fractal.js
+++ fractal.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const path = require('path');
-// const mandelbrot = require('@frctl/mandelbrot');
 const pkg = require('./package.json');
 const fractal = module.exports = require('@frctl/fractal').create();
 const webUITheme = require('mono-fractal')({
