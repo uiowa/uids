9de1fe72c62e3c90632394c732131c12028b8372
Benjamin Speare <benjamin-speare@uiowa.edu>
2020-05-27 08:40:45 -0500

    Added offset to header js to delay scroll down header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,19 +4,15 @@ const canvasLock = "o-canvas--lock";
 let lastScroll = 0;
 
 // navigation scroll
-
 window.addEventListener("scroll", function () {
   const currentScroll = window.pageYOffset;
-  if (currentScroll == 0) {
-    body.classList.remove(scrollUp);
-    body.classList.remove(scrollDown);
-    return;
-  }
-// if contains lock don't add anything....
+  // if contains lock don't add anything....
   if (currentScroll > lastScroll && !body.classList.contains(canvasLock)) {
     // down
-    body.classList.remove(scrollUp);
-    body.classList.add(scrollDown);
+    if (currentScroll > 82) {
+      body.classList.remove(scrollUp);
+      body.classList.add(scrollDown);
+    }
   } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
     // up
     body.classList.remove(scrollDown);
