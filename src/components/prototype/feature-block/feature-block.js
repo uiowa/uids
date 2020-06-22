7c242fa42314b3888c15ebfc4797efc43a62f32f
Benjamin Speare <benjamin-speare@uiowa.edu>
2020-06-22 12:23:29 -0500

    Added feature block to prototype dir

---
+++ src/components/prototype/feature-block/feature-block.js
@@ -0,0 +1,28 @@
+const elements = document.getElementsByClassName("feature");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].onmouseover = elements[i].onfocus = function () {
+    // remove attribute from sibling
+    let el = elements[0];
+    while (el) {
+      if (el.tagName === "A") {
+        //remove attribute
+        el.setAttribute("aria-expanded", "false");
+      }
+      // pass the attribute to sibling
+      el = el.nextSibling;
+    }
+    this.setAttribute("aria-expanded", "true");
+  };
+  elements[i].onmouseout = elements[i].onfocus = function () {
+    let el = elements[0];
+     while (el) {
+       if (el.tagName === "A") {
+         //remove attribute
+         el.setAttribute("aria-expanded", "false");
+       }
+       // pass the attribute to sibling
+       el = el.nextSibling;
+     }
+ elements[0].setAttribute("aria-expanded", "true");
+  };
+}
