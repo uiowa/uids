0004442ca0a0c4d8c082622f5cc4d9b2e94d16ec
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-06-23 18:04:50 -0500

    Refactored slider to get js working and to remove unnecessary styles.

--- src/components/prototype/slider/slider.js
+++ src/components/prototype/slider/slider.js
@@ -1,33 +1,19 @@
-const elements = document.getElementsByClassName("slider__slide");
+const elements = document.getElementsByClassName('slider__slide');
 console.log(elements);
 for (let i = 0; i < elements.length; i++) {
   elements[i].onmouseover = elements[i].onfocus = function () {
-    // remove attribute from sibling
-    let el = elements[0];
-    console.log(el)
-    while (el) {
-      //remove attribute
-      el.setAttribute("aria-expanded", "false");
-      // @todo Remove this if not necessary.
-      if (el.tagName === "A") {
-      }
-      // pass the attribute to sibling
-      el = el.nextSibling;
-    }
+    setAriaFalse(elements);
     this.setAttribute("aria-expanded", "true");
   };
   elements[i].onmouseout = elements[i].onfocus = function () {
-    let el = elements[0];
-    console.log(el)
-    while (el) {
-      //remove attribute
-      el.setAttribute("aria-expanded", "false");
-      // @todo Remove this if not necessary.
-      if (el.tagName === "A") {
-      }
-      // pass the attribute to sibling
-      el = el.nextSibling;
-    }
+    setAriaFalse(elements);
     elements[0].setAttribute("aria-expanded", "true");
   };
+}
+
+const setAriaFalse = function(elements) {
+  for (let i = 0; i < elements.length; i++) {
+    let el = elements.item(i);
+    el.setAttribute('aria-expanded', 'false');
+  }
 }
