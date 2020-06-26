4c02dda50d29d4a4fffce2568b787f65ae347748
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-06-25 21:46:53 -0500

    Added javascript to allow left and right arrow navigation, at least as a proof of concept.

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,15 +1,51 @@
+// @todo Add ability to have multiple sliders on a page.
 const elements = document.getElementsByClassName('slider__slide');
 
 for (let i = 0; i < elements.length; i++) {
   elements[i].onmouseover = elements[i].onfocus = function () {
-    setAriaFalse(elements);
-    this.setAttribute("aria-expanded", "true");
+    setActiveSlide(this);
   };
 }
 
-const setAriaFalse = function(elements) {
+const setAriaFalse = () => {
   for (let i = 0; i < elements.length; i++) {
     let el = elements.item(i);
     el.setAttribute('aria-expanded', 'false');
   }
+}
+
+const setActiveSlide = element => {
+  setAriaFalse(elements);
+  element.setAttribute('aria-expanded', 'true');
+}
+
+document.addEventListener('keyup', (event) => {
+  keyHandler(event);
+}, false);
+
+const keyHandler = function(event) {
+  if (event.keyCode === 37) {
+    // left - previous
+    let index = getActiveSlideIndex();
+    if (index !== false && index > 0) {
+      // @todo Allow looping?
+      setActiveSlide(elements[index-1]);
+    }
+  } else if (event.keyCode === 39) {
+    // right - next
+    let index = getActiveSlideIndex();
+    if (index !== false && index < elements.length-1) {
+      setActiveSlide(elements[index+1]);
+    }
+  }
+}
+
+const getActiveSlideIndex = () => {
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].getAttribute('aria-expanded') === 'true') {
+      return i;
+    }
+  }
+
+  return false;
 }
