e6e1919eaf54b30a5511019742ef58f7c08a1602
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-06-26 15:50:25 -0500

    Renamed or removed some scss files. Updated references. Started working on a new theme.scss implementation.

--- gulpfile.js
+++ gulpfile.js
@@ -14,87 +14,104 @@ const imagemin      = require('gulp-imagemin')
  * Directories here
  */
 var paths = {
-    build: `${__dirname}/www`,
-    src: `${__dirname}/src`,
-    dest: `${__dirname}/tmp`,
-    dist: `${__dirname}/dist`,
+  build: `${__dirname}/www`,
+  src: `${__dirname}/src`,
+  dest: `${__dirname}/tmp`,
+  dist: `${__dirname}/dist`,
+  theme: `${__dirname}/theme`,
 };
 
 // Clean
 function clean() {
-    return del(`${paths.dest}/assets/`);
+  return del(`${paths.dest}/assets/`);
 }
 
 function cleanComponentCss() {
-    return del(`${paths.src}/components/**/*.css`);
+  return del(`${paths.src}/components/**/*.css`);
 }
 
 function assetsCopy() {
-    return src([
-        `${paths.src}/assets/**/*.{woff,woff2}`,
-        `${paths.src}/assets/**/*.css`,
-        `${paths.src}/assets/**/*.js`,
-        `${paths.src}/assets/**/*.{jpg,png,svg}`,
-        `${paths.src}/assets/**/*.{mov,mp4}`,
-    ])
+  return src([
+    `${paths.src}/assets/**/*.{woff,woff2}`,
+    `${paths.src}/assets/**/*.css`,
+    `${paths.src}/assets/**/*.js`,
+    `${paths.src}/assets/**/*.{jpg,png,svg}`,
+    `${paths.src}/assets/**/*.{mov,mp4}`,
+  ])
     .pipe(dest(`${paths.dest}/assets`));
 }
 
 // Icons
 function icons() {
-    return src(`${paths.src}/assets/icons/**/*`)
-        .pipe(imagemin())
-        .pipe(dest(`${paths.dest}/assets/icons`));
+  return src(`${paths.src}/assets/icons/**/*`)
+    .pipe(imagemin())
+    .pipe(dest(`${paths.dest}/assets/icons`));
 }
 
 function componentCssInPlace() {
-    return src(`${paths.src}/components/**/*.scss`)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([autoprefixer()]))
-        .pipe(dest(`${paths.src}/components`));
+  return src(`${paths.src}/components/**/*.scss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest(`${paths.src}/components`));
 }
 
 // Styles
 function buildStyles() {
-    return src(`${paths.src}/assets/scss/*.scss`)
-        .pipe(glob())
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: [
-                `${paths.src}/components`,
-            ],
-        }).on('error', sass.logError))
-        .pipe(postcss([ autoprefixer(), cssnano()]))
-        .pipe(sourcemaps.write('./'))
-        .pipe(dest(`${paths.dest}/assets/css`));
+  return src(`${paths.src}/assets/scss/*.scss`)
+    .pipe(glob())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: [
+        `${paths.src}/components`,
+      ],
+    }).on('error', sass.logError))
+    .pipe(postcss([ autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(`${paths.dest}/assets/css`));
+}
+
+function buildTheme() {
+  return src(`${paths.theme}/assets/scss/*.scss`)
+    .pipe(glob())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: [
+        `${paths.src}/components`,
+        `${paths.src}/assets/scss`,
+      ],
+    }).on('error', sass.logError))
+    .pipe(postcss([ autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(`${paths.theme}/assets/css`));
 }
 
 function serve() {
-    const server = fractal.web.server({
-        sync: true,
-        syncOptions: {
-            https: true
-        },
-    });
-    server.on('error', err => logger.error(err.message));
-    return server.start().then(() => {
-        logger.success(`Fractal server is now running at ${server.url}`);
-    });
+  const server = fractal.web.server({
+    sync: true,
+    syncOptions: {
+      https: true
+    },
+  });
+  server.on('error', err => logger.error(err.message));
+  return server.start().then(() => {
+    logger.success(`Fractal server is now running at ${server.url}`);
+  });
 }
 
 // Watch
 function startWatch(done) {
-    serve();
-    watch(`${paths.src}/assets/icons`, icons);
-    // watch(`${paths.src}/assets/images`, images);
-    // watch(`${paths.src}/assets/vectors`, images);
-    // watch(`${paths.src}/**/*.js`, scripts);
-    watch(`${paths.src}/**/*.scss`, compile);
+  serve();
+  watch(`${paths.src}/assets/icons`, icons);
+  // watch(`${paths.src}/assets/images`, images);
+  // watch(`${paths.src}/assets/vectors`, images);
+  // watch(`${paths.src}/**/*.js`, scripts);
+  watch(`${paths.src}/**/*.scss`, compile);
+  watch(`${paths.theme}/**/*.scss`, buildTheme)
 
-    done();
+  done();
 }
 
-const styles = series(componentCssInPlace, buildStyles);
+const styles = series(componentCssInPlace, buildStyles, buildTheme);
 
 const compile = series(parallel(clean, cleanComponentCss), parallel(assetsCopy, icons, styles));
 
