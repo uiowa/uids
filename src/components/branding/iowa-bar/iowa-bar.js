a916e22ff139ba5c4749a4ecb9f059c97bd7c422
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-07-07 15:37:00 -0500

    Change _off-canvas.scss to off-canvas.scss to be able to load the CSS file from the basic page layout. Updated references to previous files. Updated all remaining references of 'heading' to 'headline'.

--- src/components/branding/iowa-bar/iowa-bar.js
+++ src/components/branding/iowa-bar/iowa-bar.js
@@ -1,49 +1,72 @@
-const scrollUp = "scroll-up";
-const scrollDown = "scroll-down";
-const canvasLock = "o-canvas--lock";
+
 const menuDrawer = document.querySelector('.header-not-sticky .o-canvas__drawer');
 const menuDrawerMobile = document.querySelector('.o-canvas__drawer');
-const header = document.querySelector('[data-uids-header]');
-const menuMq = window.matchMedia("(min-width: 855px)");
-let height = header.clientHeight;
-let lastScroll = 0;
 
-// navigation scroll
-window.addEventListener("scroll", function () {
+function StickyBar(config) {
+  if (config.offset) {
+    this.offset = config.offset;
+  } else {
+    this.offset = 0;
+  }
+  this.scrollUp = 'scroll-up';
+  this.scrollDown = 'scroll-down';
+  this.lastScroll = 0;
+
+  // navigation scroll
+  window.addEventListener('scroll', this.scrollHandler);
+}
+
+StickyBar.prototype.scrollHandler = function() {
+  const body = document.body;
   const currentScroll = window.pageYOffset;
   // remove classes if scrolled all the way up
-  if (currentScroll <= height) {
-    body.classList.remove(scrollUp);
-    body.classList.remove(scrollDown);
+  if (currentScroll <= this.offset) {
+    body.classList.remove(this.scrollUp);
+    body.classList.remove(this.scrollDown);
+    // @todo Move this to off-canvas.
     if (menuMq.matches) {
       if (menuDrawer) {
-        menuDrawer.style.top = Math.max(height - this.scrollY) + 'px';
+        menuDrawer.style.top = Math.max(this.offset - this.scrollY) + 'px';
       }
     } else {
-      menuDrawerMobile.style.top = Math.max(height - this.scrollY) + 'px';
+      menuDrawerMobile.style.top = Math.max(this.offset - this.scrollY) + 'px';
     }
     return;
   }
   // if contains lock don't add anything....
-  if (currentScroll > lastScroll && !body.classList.contains(canvasLock)) {
+  if (currentScroll > this.lastScroll && !body.classList.contains('o-canvas--lock')) {
+    console.log('scrolling down');
     // down
-    if (currentScroll > height) {
-      body.classList.remove(scrollUp);
-      body.classList.add(scrollDown);
+    if (currentScroll > this.offset) {
+      body.classList.remove(this.scrollUp);
+      body.classList.add(this.scrollDown);
     }
-  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+  } else if (currentScroll < this.lastScroll && body.classList.contains(this.scrollDown)) {
+    console.log('scrolling up');
     // up
-    body.classList.remove(scrollDown);
-    body.classList.add(scrollUp);
+    body.classList.remove(this.scrollDown);
+    body.classList.add(this.scrollUp);
   }
-  lastScroll = currentScroll;
-});
+  this.lastScroll = currentScroll;
+}
 
-window.addEventListener("orientationchange", function (event) {
-  var afterOrientationChange = function () {
+window.addEventListener('orientationchange', function (event) {
+  const afterOrientationChange = function () {
     menuDrawerMobile.style.top = Math.max(header.offsetHeight - this.scrollY) + 'px';
     window.removeEventListener('resize', afterOrientationChange);
   };
   window.addEventListener('resize', afterOrientationChange);
   return;
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.querySelector('[data-uids-header]');
+  const menuMq = window.matchMedia('(min-width: 855px)');
+
+  // @todo Add checking to make sure header exists.
+  if (header) {
+    window.StickyBar = new StickyBar({
+      offset: header.clientHeight,
+    });
+  }
+}, false);
