cc4c14cf388fb69783b0c59674760a81bc51c0b2
pyrello <sean-adams-hiett@uiowa.edu>
2020-07-08 14:46:14 +0000

    Deploying to gh-pages from  @ 91269e9577a3a263620bc7c4772206dd5c7ac3e1 🚀

--- branches/feature_uids_3_0_updates_fixes/components/raw/iowa-bar/iowa-bar.js
+++ branches/feature_uids_3_0_updates_fixes/components/raw/iowa-bar/iowa-bar.js
@@ -1,54 +1,42 @@
-
+const scrollUp = 'scroll-up';
+const scrollDown = 'scroll-down';
 const menuDrawer = document.querySelector('.header-not-sticky .o-canvas__drawer');
 const menuDrawerMobile = document.querySelector('.o-canvas__drawer');
+const header = document.querySelector('[data-uids-header]');
+const menuMq = window.matchMedia('(min-width: 855px)');
+let height = header.clientHeight;
+let lastScroll = 0;
 
-function StickyBar(config) {
-  if (config.offset) {
-    this.offset = config.offset;
-  } else {
-    this.offset = 0;
-  }
-  this.scrollUp = 'scroll-up';
-  this.scrollDown = 'scroll-down';
-  this.lastScroll = 0;
-
-  // navigation scroll
-  window.addEventListener('scroll', this.scrollHandler);
-}
-
-StickyBar.prototype.scrollHandler = function() {
-  const body = document.body;
+// navigation scroll
+window.addEventListener('scroll', function () {
   const currentScroll = window.pageYOffset;
   // remove classes if scrolled all the way up
-  if (currentScroll <= this.offset) {
-    body.classList.remove(this.scrollUp);
-    body.classList.remove(this.scrollDown);
-    // @todo Move this to off-canvas.
+  if (currentScroll <= height) {
+    body.classList.remove(scrollUp);
+    body.classList.remove(scrollDown);
     if (menuMq.matches) {
       if (menuDrawer) {
-        menuDrawer.style.top = Math.max(this.offset - this.scrollY) + 'px';
+        menuDrawer.style.top = Math.max(height - this.scrollY) + 'px';
       }
     } else {
-      menuDrawerMobile.style.top = Math.max(this.offset - this.scrollY) + 'px';
+      menuDrawerMobile.style.top = Math.max(height - this.scrollY) + 'px';
     }
     return;
   }
   // if contains lock don't add anything....
-  if (currentScroll > this.lastScroll && !body.classList.contains('o-canvas--lock')) {
-    console.log('scrolling down');
+  if (currentScroll > lastScroll && !body.classList.contains('o-canvas--lock')) {
     // down
-    if (currentScroll > this.offset) {
-      body.classList.remove(this.scrollUp);
-      body.classList.add(this.scrollDown);
+    if (currentScroll > height) {
+      body.classList.remove(scrollUp);
+      body.classList.add(scrollDown);
     }
-  } else if (currentScroll < this.lastScroll && body.classList.contains(this.scrollDown)) {
-    console.log('scrolling up');
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
     // up
-    body.classList.remove(this.scrollDown);
-    body.classList.add(this.scrollUp);
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
   }
-  this.lastScroll = currentScroll;
-}
+  lastScroll = currentScroll;
+});
 
 window.addEventListener('orientationchange', function (event) {
   const afterOrientationChange = function () {
@@ -58,15 +46,3 @@ window.addEventListener('orientationchange', function (event) {
   window.addEventListener('resize', afterOrientationChange);
   return;
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  const header = document.querySelector('[data-uids-header]');
-  const menuMq = window.matchMedia('(min-width: 855px)');
-
-  // @todo Add checking to make sure header exists.
-  if (header) {
-    window.StickyBar = new StickyBar({
-      offset: header.clientHeight,
-    });
-  }
-}, false);
