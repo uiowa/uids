7e31fbfb77c4ed448c486ecdd9b6be11b7ffa59d
GaryRidgway <gary-ridgway@uiowa.edu>
2020-07-31 13:51:24 +0000

    Deploying to gh-pages from  @ 36ccaee7b7ac7c6220d777ce1d7ae11f25b0cab9 🚀

--- branches/feature_admissions_viewbook/components/raw/vertical-dots/vertical-dots.js
+++ branches/feature_admissions_viewbook/components/raw/vertical-dots/vertical-dots.js
@@ -1,3 +1,8 @@
-$('#"vertical-side-navigation" li a').click(function () {
-    // Scroll to section.
+
+// This should smoothly scroll to the top.
+$('#vertical-dots li a').click(function () {
+    let section_id = $(this).attr('data-section');
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#" + section_id).offset().top
+    }, 1200);
 });
