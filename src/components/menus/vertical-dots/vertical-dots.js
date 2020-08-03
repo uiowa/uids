63a1c6ac8d75c7441e108bde3cf81e467b56934c
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2020-08-03 16:39:05 -0500

    added pure css version of the scrolling with focus support

--- src/components/menus/vertical-dots/vertical-dots.js
+++ src/components/menus/vertical-dots/vertical-dots.js
@@ -1,8 +1,8 @@
 
-// This should smoothly scroll to the top.
-$('#vertical-dots li a').click(function () {
-    let section_id = $(this).attr('data-section');
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#" + section_id).offset().top
-    }, 1200);
-});
+// // This should smoothly scroll to the top.
+// $('#vertical-dots li a').click(function () {
+//     let section_id = $(this).attr('data-section');
+//     $([document.documentElement, document.body]).animate({
+//         scrollTop: $("#" + section_id).offset().top
+//     }, 1200);
+// });
