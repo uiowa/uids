6efd1808bf1e432c37ad5acd0f0d15d2b53025ee
GaryRidgway <gary-ridgway@uiowa.edu>
2020-08-04 19:32:20 +0000

    Deploying to gh-pages from  @ 7e974a3fb97daccf30f83e20c58939279f632991 🚀

--- branches/feature_admissions_viewbook/components/menus/vertical-dots/vertical-dots.js
+++ branches/feature_admissions_viewbook/components/menus/vertical-dots/vertical-dots.js
@@ -1,8 +1,21 @@
+let mainNavLinks = document.querySelectorAll("nav ul li a.v-dot-link");
+console.log(mainNavLinks);
 
-// This should smoothly scroll to the top.
-$('#vertical-dots li a').click(function () {
-    let section_id = $(this).attr('data-section');
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#" + section_id).offset().top
-    }, 1200);
+window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY;
+
+    mainNavLinks.forEach(link => {
+        let section = document.querySelector(link.hash).parentNode.nextElementSibling;
+        // console.log(section.offsetTop);
+        // console.log([section.offsetTop <= fromTop, section.offsetTop + section.offsetHeight > fromTop, [section.offsetTop, section.offsetHeight, fromTop]]);
+
+        if (
+            section.offsetTop -50 <= fromTop &&
+            section.offsetTop -50 + section.offsetHeight > fromTop
+        ) {
+            link.classList.add("current");
+        } else {
+            link.classList.remove("current");
+        }
+    });
 });
