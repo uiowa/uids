88d038cccb3d2719d2de00d694b3bb5161ea7470
Benjamin Speare <benjamin-speare@uiowa.edu>
2020-08-21 13:43:28 -0500

    Changed names in banner component

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,19 +1,19 @@
-const heroes = document.querySelectorAll('.hero');
+const banners = document.querySelectorAll('.banner');
 const link_elements = ['.bold-headline a', 'a.bttn'];
-Array.prototype.forEach.call(heroes, hero => {
+Array.prototype.forEach.call(banners, banner => {
   let up, down, link, i;
   // Loop through options and break on the first match.
   for (i = 0; i < link_elements.length; i++) {
-    if (link = hero.querySelector(link_elements[i])) {
+    if (link = banner.querySelector(link_elements[i])) {
       break;
     }
   }
 
   // If we have a match, attach behaviors.
   if (link) {
-    hero.style.cursor = 'pointer';
-    hero.onmousedown = () => down = +new Date();
-    hero.onmouseup = () => {
+    banner.style.cursor = 'pointer';
+    banner.onmousedown = () => down = +new Date();
+    banner.onmouseup = () => {
       up = +new Date();
       // Trigger click event if the duration is short enough.
       if ((up - down) < 200) {
