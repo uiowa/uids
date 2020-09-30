8820c6903afa57747b273cde7249b14a4d01967b
stomblin <Steven-Tomblin@uiowa.edu>
2020-09-30 11:54:40 -0500

    font tweaks

--- src/components/proposals/brand/navbar/navbar.js
+++ src/components/proposals/brand/navbar/navbar.js
@@ -14,26 +14,45 @@
   var proto = NavBar.prototype;
   var responsive;
   var openDropdown;
+  var selectedDropdown;
+
 
   function mediaqueryresponse(mql){
     if (mql.matches){ // if media query matches
+
      console.log("The condition " + mql.media + " has been met")
      document.querySelector(".site-links").setAttribute("style", "top: "+0+"px;")
 
+     if(selectedDropdown){
+      selectedDropdown.getElementsByClassName('fas')[0].className = 'fas fa-chevron-down'
+     }
+
      if(openDropdown){
       document.getElementById(openDropdown).className= "dropdown-content";
-      responsive = false;
+
+      openDropdown = undefined;
+      selectedDropdown = undefined;
      }
+     responsive = false;
     }
     else{
+
+      if(selectedDropdown){
+        selectedDropdown.getElementsByClassName('fas')[0].className = 'fas fa-chevron-down'
+       }
 
       if(openDropdown){
         document.getElementById(openDropdown).className= "dropdown-content";
         responsive = false;
        }
      console.log("Condition not met yet hide the links")
+
      document.querySelector(".site-links").className = "site-links";
+
      responsive = true;
+     openDropdown = undefined;
+     selectedDropdown = undefined;
+
     }
    }
 
@@ -62,30 +81,79 @@
 
   proto.dropDown = function dropDown(evt, id) {
 
+    var body = document.querySelector('body');
+    var nav = document.querySelector('nav');
+    console.log(body.clientWidth);
+    console.log(nav.clientWidth);
 
-    if(openDropdown){
+    var diff = body.clientWidth - nav.clientWidth;
+
+    console.log(diff);
+    // get width of the entire nav
+
+    // get width of the body
+
+    // if body and nav are not the same
+
+    // subtract and shift the dropdown over to the right
+
+    // get width of the button
+
+    // get width of the dropdown
+
+    // if the
+
+
+    // same button clicked, toggle close
+    if(openDropdown === id){
       document.getElementById(openDropdown).className= "dropdown-content";
-     }
+      // get the parent button and highlight
+      selectedDropdown.className= "dropbtn";
+      selectedDropdown.getElementsByClassName('fas')[0].className = 'fas fa-chevron-down'
+      openDropdown = undefined;
+      selectedDropdown = undefined;
+    }else{
 
-    openDropdown = id;
-    var h = evt.offsetHeight - 3; // padding
-    console.log(h);
-    console.log(responsive);
-    var x = document.getElementById(id);
-    if (x.className === "dropdown-content") {
-      x.className += " open";
-      if(!responsive){
-        x.setAttribute("style", "top: "+h+"px;");
-      }else{
-      x.setAttribute("style", "top: "+0+"px;");
+      // now check to see if another drop down is open
+      if(openDropdown){
+        document.getElementById(openDropdown).className= "dropdown-content";
+
       }
-     // x.hidden = false;
-      evt.setAttribute("aria-expanded", true);
-    } else {
-      x.className = "dropdown-content";
-     // x.hidden = true;
-      evt.setAttribute("aria-expanded", false);
+      if(selectedDropdown){
+        selectedDropdown.className= "dropbtn";
+        selectedDropdown.getElementsByClassName('fas')[0].className = 'fas fa-chevron-down'
+      }
+
+      openDropdown = id;
+      selectedDropdown= evt;
+      var h = evt.offsetHeight ; // padding
+      console.log(h);
+      console.log('responsive'+responsive);
+      var x = document.getElementById(id);
+      if (x.className === "dropdown-content") {
+        x.className += " open";
+        if(!responsive){
+          x.setAttribute("style", "top: "+h+"px;");
+        }else{
+        x.setAttribute("style", "top: "+0+"px;");
+        }
+      // x.hidden = false;
+        evt.setAttribute("aria-expanded", true);
+        evt.className= "dropbtn dropdown__selected";
+        //flip the chevron
+
+        // evt.getElementsByClassName('fa-chevron-down').className = 'fas fa-chevron-up';
+        evt.getElementsByClassName('fas')[0].className = 'fas fa-chevron-up'
+      } else {
+        x.className = "dropdown-content";
+      // x.hidden = true;
+        evt.setAttribute("aria-expanded", false);
+        evt.getElementsByClassName('fas')[0].className = 'fas fa-chevron-down'
+        // get the parent button and highlight
+        evt.className= "dropbtn";
+      }
     }
+
   }
 //https://oli.me.uk/exporting-through-amd-commonjs-and-the-global-object/
   // Expose the class either via AMD, CommonJS or the global object
