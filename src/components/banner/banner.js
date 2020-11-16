059bc7fb46bce990153520b1afdd311e88abb2d0
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2020-11-16 16:41:03 -0600

    adding multiple variant for testing

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -22,3 +22,45 @@ Array.prototype.forEach.call(banners, banner => {
     }
   }
 });
+
+
+
+
+// Banner Video cut straight from hero, needs refactoring for multiple banner instances.
+const video = document.getElementById("video-container");
+const btn = document.getElementById("video-btn");
+const motionQuery = matchMedia('(prefers-reduced-motion)');
+
+function reducedMotionCheck() {
+  if (motionQuery.matches) {
+    video.pause();
+    btn.innerHTML = "<span class='element-invisible'>" + "Play" + "</span>";
+    btn.classList.remove("video-btn__pause");
+    btn.classList.add("video-btn__play");
+    btn.setAttribute("aria-label", "Play");
+  }
+}
+
+reducedMotionCheck();
+motionQuery.addListener(reducedMotionCheck);
+
+if (document.getElementById("video-btn")) {
+  document.getElementById("video-btn").addEventListener("click", pausePlay);
+}
+
+// Pause and play
+function pausePlay() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "<span class='element-invisible'>" + "Pause" + "</span>";
+    btn.classList.remove("video-btn__play");
+    btn.classList.add("video-btn__pause");
+    btn.setAttribute("aria-label", "Pause");
+  } else {
+    video.pause();
+    btn.innerHTML = "<span class='element-invisible'>" + "Play" + "</span>";
+    btn.classList.remove("video-btn__pause");
+    btn.classList.add("video-btn__play");
+    btn.setAttribute("aria-label", "Play");
+  }
+}
