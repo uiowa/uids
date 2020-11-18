7b47ee46eeeb5c5854776f6367b88d35b65a5785
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2020-11-18 09:24:06 -0600

    fixing video selector

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -38,7 +38,7 @@ const motionQuery = matchMedia('(prefers-reduced-motion)');
 // Construct an array of all banner videos.
 let banner_videos = [];
 banners.forEach(function(item, index) {
-  banner_videos.push(item.querySelector('.banner video'));
+  banner_videos.push(item.querySelector('video'));
 });
 
 // For each banner video...
