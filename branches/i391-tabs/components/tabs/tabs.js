c28f2ba87f0c155ed7f6bd53eb9841afacf5f956
pyrello <sean-adams-hiett@uiowa.edu>
2020-12-18 20:41:32 +0000

    Deploying to gh-pages from  @ e20164d695af0a0e837ab6d1d548af5717a33e25 🚀

--- branches/i391-tabs/components/tabs/tabs.js
+++ branches/i391-tabs/components/tabs/tabs.js
@@ -6,7 +6,7 @@
 */
 (function () {
   // For easy reference.
-  let keys = {
+  const keys = {
     end: 35,
     home: 36,
     left: 37,
@@ -17,103 +17,62 @@
   };
 
   // Add or subtract depending on key pressed.
-  let direction = {
+  const direction = {
     37: -1,
     38: -1,
     39: 1,
     40: 1
   };
 
-  // Select all 'Tabs' components.
-  let tabs_elements = document.querySelectorAll('.tabs');
-  // Define tabs_array_dict for later use.
-  let tabs_array_dict = [];
   // this delay could be set by a function later if ever needed.
-  let delay = 0;
+  const delay = 0;
 
-  function Tabs() {
-    // Generate array of Tabs elements dictionaries.
-    // This way we have any Tabs component data at any time.
-    this.generateArrays();
-    // Functionally guarantee that no tabs have any control naming collision.
-    this.distinctifyTabs();
-    // Bind listeners
-    this.addListeners();
+  function Tabs(element) {
+    if (element) {
+      if (!element.hasAttribute('id')) {
+        console.warn('[UIDS] Tabs (<div class="tab">) needs unique ID to function correctly.')
+      }
+      // Set references to tab elements.
+      this.tablist = element.querySelectorAll('[role="tablist"]')[0];
+      this.tabs = element.querySelectorAll('[role="tab"]');
+      this.panels = element.querySelectorAll('[role="tabpanel"]');
+      // Bind listeners
+      this.addListeners();
+    }
   }
-
-  // THis function generates the array of Tabs elements dictionaries.
-  Tabs.prototype.generateArrays = function() {
-    // For each .tabs element...
-    tabs_elements.forEach(function(item, index) {
-      // Give it a distinct ID...
-      item.id = 'tabs--' + index;
-      // Query the necessary nodelists from them...
-      let tablist = item.querySelectorAll('[role="tablist"]')[0];
-      let tabs = item.querySelectorAll('[role="tab"]');
-      let panels = item.querySelectorAll('[role="tabpanel"]');
-      // Put all of them in to an indexable dictionary and push that to the tabs_array_dict array.
-      tabs_array_dict.push(
-        {
-          tablist: tablist,
-          tabs:    tabs,
-          panels:  panels
-        }
-      );
-    });
-  };
-
-  // This function guarantees that no tabs have any control naming collision.
-  Tabs.prototype.distinctifyTabs = function() {
-    // For each Tabs component in the tabs_array_dict...
-    tabs_array_dict.forEach(function(tabgroup, tabgroupIndex) {
-      // For each tab defined in that component...
-      tabgroup.tabs.forEach(function(tab) {
-        // Give it an ID prefix based upon the Order the currentTab component is in the DOM.
-        tab.id = 'tabgroup--' + tabgroupIndex + '-' + tab.id;
-        // Make sure the Reference to the Panel it is linked to also has the prefix.
-        tab.setAttribute('aria-controls','tabgroup--' + tabgroupIndex + '-' + tab.getAttribute('aria-controls'));
-      });
-
-      // For each panel defined in that component...
-      tabgroup.panels.forEach(function(panel) {
-        // Give it an ID prefix based upon the Order the currentTab component is in the DOM.
-        panel.id = 'tabgroup--' + tabgroupIndex + '-' + panel.id;
-        // Make sure the reference to the Tab it is linked to also has the prefix.
-        panel.setAttribute('aria-labelledby','tabgroup--' + tabgroupIndex + '-' + panel.getAttribute('aria-labelledby'));
-      });
-    });
-  };
 
   // This function adds listeners for events to every tab.
   Tabs.prototype.addListeners = function() {
-    // For each Tabs component in the tabs_array_dict...
-    tabs_array_dict.forEach(function(item) {
-      // Set listeneners for all three necessary event types.
-      let tabs = item.tabs;
-      for (let i = 0; i < item.tabs.length; ++i) {
-        tabs[i].addEventListener('click', clickEventListener);
-        tabs[i].addEventListener('keydown', keydownEventListener);
-        tabs[i].addEventListener('keyup', keyupEventListener);
+    // Set listeners for all three necessary event types.
+    for (let i = 0; i < this.tabs.length; ++i) {
+      this.tabs[i].addEventListener('click', event => {
+        this.clickEventListener(event);
+      });
+      this.tabs[i].addEventListener('keydown', event => {
+        this.keydownEventListener(event);
+      });
+      this.tabs[i].addEventListener('keyup', event => {
+        this.keyupEventListener(event);
+      });
 
-        // Build an array with all tabs (<button>s) in it.
-        tabs[i].index = i;
-      }
-    });
+      // Build an array with all tabs (<button>s) in it.
+      this.tabs[i].index = i;
+    }
   };
 
   // When a tab is clicked, activateTab is fired to activate it.
-  function clickEventListener(event) {
-    let tab = event.target;
-    activateTab(tab, false);
+  Tabs.prototype.clickEventListener = function(event) {
+    console.log(this);
+    const tab = event.target;
+    this.activateTab(tab, false);
   }
 
   // This function activates any given tab panel.
-  function activateTab(tab, setFocus) {
+  Tabs.prototype.activateTab = function(tab, setFocus) {
     setFocus = setFocus || true;
-    let parent = tab.parentElement.parentElement;
-    let tab_group_id = parent.id.split('--')[parent.id.split('--').length-1];
+
     // Deactivate all other tabs.
-    deactivateTabs(tab_group_id);
+    this.deactivateTabs();
 
     // Remove tabindex attribute.
     tab.removeAttribute('tabindex');
@@ -134,79 +93,78 @@
   }
 
   // Deactivate all tabs and tab panels.
-  function deactivateTabs(tab_group_id) {
+  Tabs.prototype.deactivateTabs = function() {
       // Get the necessary tabs nodelist.
-      let tabs = tabs_array_dict[tab_group_id].tabs;
+      let tabs = this.tabs;
       // For each tab in tabs...
       for (let t = 0; t < tabs.length; t++) {
         // Remove the necessary accessibility attributes.
         tabs[t].setAttribute('tabindex', '-1');
         tabs[t].setAttribute('aria-selected', 'false');
-        tabs[t].removeEventListener('focus', focusEventHandler);
+        tabs[t].removeEventListener('focus', this.focusEventHandler);
       }
 
       // Get the necessary panels nodelist.
-      let panels = tabs_array_dict[tab_group_id].panels;
       // For each panel in panels...
-      for (let p = 0; p < panels.length; p++) {
+      for (let p = 0; p < this.panels.length; p++) {
         // Set the hidden attribute so that it cant be seen.
-        panels[p].setAttribute('hidden', 'hidden');
+        this.panels[p].setAttribute('hidden', 'hidden');
       }
   }
 
   // This function will, on an event, check if the event target needs to be focused.
-  function focusEventHandler(event) {
+  Tabs.prototype.focusEventHandler = function(event) {
     let target = event.target;
-    setTimeout(checkTabFocus, delay, target);
+    const that = this;
+    setTimeout(function() {
+      that.checkTabFocus(event);
+    }, delay, target);
   }
 
   // Only activate tab on focus if it still has focus after the delay.
-  function checkTabFocus(target) {
+  Tabs.prototype.checkTabFocus = function(target) {
     let focused = document.activeElement;
 
     if (target === focused) {
-      activateTab(target, false);
+      this.activateTab(target, false);
     }
   }
 
   // Handle keydown on tabs.
-  function keydownEventListener(event) {
+  Tabs.prototype.keydownEventListener = function(event) {
     let key = event.keyCode;
-    // Get the tab_group_id for later use.
-    let parent = event.target.parentElement.parentElement;
-    let tab_group_id = parent.id.split('--')[parent.id.split('--').length-1];
     // Get the correct tabs nodelist.
-    let tabs = tabs_array_dict[tab_group_id].tabs;
+    let tabs = this.tabs;
 
     switch (key) {
       case keys.end:
         event.preventDefault();
         // Activate last tab.
-        activateTab(tabs[tabs.length - 1]);
+        this.activateTab(tabs[tabs.length - 1]);
         break;
       case keys.home:
         event.preventDefault();
         // Activate first tab.
-        activateTab(tabs[0]);
+        this.activateTab(tabs[0]);
         break;
 
       // Up and down are in keydown.
       // Because we need to prevent page scroll.
       case keys.up:
       case keys.down:
-        determineOrientation(event);
+        this.determineOrientation(event);
         break;
     }
   }
 
   // Handle keyup on tabs.
-  function keyupEventListener(event) {
+  Tabs.prototype.keyupEventListener = function(event) {
     let key = event.keyCode;
 
     switch (key) {
       case keys.left:
       case keys.right:
-        determineOrientation(event);
+        this.determineOrientation(event);
         break;
     }
   }
@@ -214,13 +172,11 @@
   // When a tablist's aria-orientation is set to vertical,
   // only up and down arrow should function.
   // In all other cases only left and right arrow function.
-  function determineOrientation(event) {
+  Tabs.prototype.determineOrientation = function(event) {
     let key = event.keyCode;
-    // Get the tab_group_id for later use.
-    let parent = event.target.parentElement.parentElement;
-    let tab_group_id = parent.id.split('--')[parent.id.split('--').length-1];
+
     // Get the correct tablist nodelist.
-    let tablist = tabs_array_dict[tab_group_id].tablist;
+    let tablist = this.tablist;
     // Determine the tab orientation.
     let vertical = tablist.getAttribute('aria-orientation') === 'vertical';
     let proceed = false;
@@ -238,24 +194,23 @@
     }
 
     if (proceed) {
-      switchTabOnArrowPress(event);
+      this.switchTabOnArrowPress(event);
     }
   }
 
   // Either focus the next, previous, first, or last tab
   // depening on key pressed
-  function switchTabOnArrowPress(event) {
+  Tabs.prototype.switchTabOnArrowPress = function(event) {
     let pressed = event.keyCode;
-    // Get the tab_group_id for later use.
-    let parent = event.target.parentElement.parentElement;
-    let tab_group_id = parent.id.split('--')[parent.id.split('--').length-1];
     // Get the correct tabs nodelist.
-    let tabs = tabs_array_dict[tab_group_id].tabs;
+    let tabs = this.tabs;
 
     // For each tab in tabs...
     for (let x = 0; x < tabs.length; x++) {
       // Add a focus event handler.
-      tabs[x].addEventListener('focus', focusEventHandler);
+      tabs[x].addEventListener('focus', event => {
+        this.focusEventHandler(event);
+      });
     }
 
     // If a pressed key is in the direction array.
@@ -297,6 +252,12 @@
 
   // Create an object to avoid collision.
   window.UidsTabs = Tabs;
-  // Instantiate the object.
-  new UidsTabs();
+
+  // Instantiate videos on the page.
+  const items = document.getElementsByClassName('tabs');
+
+  for (let i = 0; i < items.length; i++) {
+    new UidsTabs(items[i], i);
+  }
+
 }());
