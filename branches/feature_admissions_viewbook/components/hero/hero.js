185e894f6f80850b96492f8638a361fb8b8e06b1
pyrello <sean-adams-hiett@uiowa.edu>
2021-01-06 22:28:18 +0000

    Deploying to gh-pages from  @ 50dc2bc67c32b0541ba46d3087a9119e42acb9c6 🚀

--- branches/feature_admissions_viewbook/components/hero/hero.js
+++ branches/feature_admissions_viewbook/components/hero/hero.js
@@ -1,24 +1,37 @@
-const heroes = document.querySelectorAll('.hero');
-const link_elements = ['.bold-headline a', 'a.bttn'];
-Array.prototype.forEach.call(heroes, hero => {
-  let up, down, link, i;
-  // Loop through options and break on the first match.
-  for (i = 0; i < link_elements.length; i++) {
-    if (link = hero.querySelector(link_elements[i])) {
-      break;
-    }
-  }
+const video = document.getElementById("video-container");
+const btn = document.getElementById("video-btn");
+const motionQuery = matchMedia('(prefers-reduced-motion)');
 
-  // If we have a match, attach behaviors.
-  if (link) {
-    hero.style.cursor = 'pointer';
-    hero.onmousedown = () => down = +new Date();
-    hero.onmouseup = () => {
-      up = +new Date();
-      // Trigger click event if the duration is short enough.
-      if ((up - down) < 200) {
-        link.click();
-      }
-    }
+function reducedMotionCheck() {
+  if (motionQuery.matches) {
+    video.pause();
+    btn.innerHTML = "<span class='element-invisible'>" + "Play" + "</span>";
+    btn.classList.remove("video-btn__pause");
+    btn.classList.add("video-btn__play");
+    btn.setAttribute("aria-label", "Play");
   }
-});
+}
+
+reducedMotionCheck();
+motionQuery.addListener(reducedMotionCheck);
+
+if (document.getElementById("video-btn")) {
+  document.getElementById("video-btn").addEventListener("click", pausePlay);
+}
+
+// Pause and play
+function pausePlay() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "<span class='element-invisible'>" + "Pause" + "</span>";
+    btn.classList.remove("video-btn__play");
+    btn.classList.add("video-btn__pause");
+    btn.setAttribute("aria-label", "Pause");
+  } else {
+    video.pause();
+    btn.innerHTML = "<span class='element-invisible'>" + "Play" + "</span>";
+    btn.classList.remove("video-btn__pause");
+    btn.classList.add("video-btn__play");
+    btn.setAttribute("aria-label", "Play");
+  }
+}
