0d8a0a54e07a79c5077c5ccb88e2337eccd36be9
GaryRidgway <GaryRidgway@users.noreply.github.com>
2021-03-23 15:32:22 +0000

    Deploying to gh-pages from  @ 4a405865683a7544ca169daaef270ccae9f3fc35 🚀

---
+++ branches/border-hotfix/components/branding/iowa-bar/iowa-bar.js
@@ -0,0 +1,48 @@
+const scrollUp = 'scroll-up';
+const scrollDown = 'scroll-down';
+const menuDrawer = document.querySelector('.header-not-sticky .o-canvas__drawer');
+const menuDrawerMobile = document.querySelector('.o-canvas__drawer');
+const header = document.querySelector('[data-uids-header]');
+const menuMq = window.matchMedia('(min-width: 855px)');
+let height = header.clientHeight;
+let lastScroll = 0;
+
+// navigation scroll
+window.addEventListener('scroll', function () {
+  const currentScroll = window.pageYOffset;
+  // remove classes if scrolled all the way up
+  if (currentScroll <= height) {
+    body.classList.remove(scrollUp);
+    body.classList.remove(scrollDown);
+    if (menuMq.matches) {
+      if (menuDrawer) {
+        menuDrawer.style.top = Math.max(height - this.scrollY) + 'px';
+      }
+    } else {
+      menuDrawerMobile.style.top = Math.max(height - this.scrollY) + 'px';
+    }
+    return;
+  }
+  // if contains lock don't add anything....
+  if (currentScroll > lastScroll && !body.classList.contains('o-canvas--lock')) {
+    // down
+    if (currentScroll > height) {
+      body.classList.remove(scrollUp);
+      body.classList.add(scrollDown);
+    }
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
+
+window.addEventListener('orientationchange', function (event) {
+  const afterOrientationChange = function () {
+    menuDrawerMobile.style.top = Math.max(header.offsetHeight - this.scrollY) + 'px';
+    window.removeEventListener('resize', afterOrientationChange);
+  };
+  window.addEventListener('resize', afterOrientationChange);
+  return;
+});
