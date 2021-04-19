4e2a4e8e8a25315ba69b8f12642fa5810a45c824
bspeare <bspeare@users.noreply.github.com>
2021-04-19 14:01:06 -0500

    Feature: Added data attribute for removing entire card link function in card.js (#532)
    * Added data attribute for removing entire card link function in card.js
    
    * Updated config
    
    * updated formatting
    
    * Removed button text from example
    
    * Updated card_no_link to boolean
    
    * Updated template to use actual link instead of faux link if card_no_link is set
    
    * Removed extra spaces. Simplified logic.
    
    Co-authored-by: Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,5 +1,6 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card:not([data-uids-no-link])');
 const link_elements = ['.card__title a', '.card__media a', 'a.bttn'];
+
 Array.prototype.forEach.call(cards, card => {
   let up, down, link, i;
   // Loop through options and break on the first match.
@@ -10,6 +11,7 @@ Array.prototype.forEach.call(cards, card => {
   }
 
   // If we have a match, attach behaviors.
+
   if (link) {
     card.style.cursor = 'pointer';
     card.onmousedown = () => down = +new Date();
