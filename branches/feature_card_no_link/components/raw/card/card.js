ff777253a5e36f82d05eb80a5572b2abf2de12d8
bspeare <bspeare@users.noreply.github.com>
2021-04-19 15:22:20 +0000

    Deploying to gh-pages from  @ 256be8db09671c0734e180ccb76ababfdcb9db4d 🚀

--- branches/feature_card_no_link/components/raw/card/card.js
+++ branches/feature_card_no_link/components/raw/card/card.js
@@ -5,22 +5,22 @@ Array.prototype.forEach.call(cards, card => {
   let up, down, link, i;
   // Loop through options and break on the first match.
   for (i = 0; i < link_elements.length; i++) {
-      if (link = card.querySelector(link_elements[i])) {
-        break;
-      }
+    if (link = card.querySelector(link_elements[i])) {
+      break;
+    }
   }
 
   // If we have a match, attach behaviors.
-    if (link) {
-      card.style.cursor = 'pointer';
-      card.onmousedown = () => down = +new Date();
-      card.onmouseup = () => {
-        up = +new Date();
-        // Trigger click event if the duration is short enough.
-        if ((up - down) < 200) {
-          link.click();
-        }
+
+  if (link) {
+    card.style.cursor = 'pointer';
+    card.onmousedown = () => down = +new Date();
+    card.onmouseup = () => {
+      up = +new Date();
+      // Trigger click event if the duration is short enough.
+      if ((up - down) < 200) {
+        link.click();
       }
     }
+  }
 });
-
