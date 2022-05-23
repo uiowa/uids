55f4edd7ada70d0f0fa93854f85180d5fd961467
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2022-05-23 14:27:12 -0500

    Trying something out.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,31 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "UIDS",
+      fileName: (format) => `uids.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        },
+      },
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
