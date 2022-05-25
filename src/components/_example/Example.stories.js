68c8734f25d2f384980f918a32b9659d4df4435b
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2022-05-25 13:51:57 -0500

    Added a (hidden) example component to use as the starting point for creating new components.

---
+++ src/components/_example/Example.stories.js
@@ -0,0 +1,38 @@
+import UidsExample from './Example.vue';
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Basic/Button',
+  component: UidsExample,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    excited: {
+      control: { type: 'boolean' },
+    },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { UidsExample },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<uids-example :excited="args.example">{{ args.default }}</uids-example>',
+});
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Default.args = {
+  label: 'Hello world',
+  excited: false,
+};
+
+export const Excited = Template.bind({});
+Excited.args = {
+  ...Default.args,
+  excited: true,
+};
---
+++ src/components/_example/Example.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import './example.scss';
+const name = 'uids-example'
+const props = defineProps({
+  /**
+   * A property to determine whether the message in the component is emphasized or not.
+   */
+  excited: {
+    type: Boolean,
+    default: false,
+  },
+  /**
+   * You can add additional props here.
+   */
+})
+</script>
+
+<template>
+  <div class="example">
+    <!-- @slot The default slot where the message will be displayed. -->
+    <slot>Howdy</slot><template v-if="excited">!</template>
+  </div>
+</template>
