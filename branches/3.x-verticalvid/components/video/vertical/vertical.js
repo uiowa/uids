c005af761c5e07bdb71a7b7a5a83017990a29776
GaryRidgway <GaryRidgway@users.noreply.github.com>
2022-06-17 21:13:59 +0000

    Deploying to gh-pages from @ uiowa/uids@771cddebb17ed7ef9c56d60a0c6d8632a3fb30c7 🚀

--- branches/3.x-verticalvid/components/video/vertical/vertical.js
+++ branches/3.x-verticalvid/components/video/vertical/vertical.js
@@ -2,19 +2,35 @@ const ctrlVideo = document.querySelectorAll(".player");
 
 for (i = 0; i < ctrlVideo.length; ++i) {
   ctrlVideo[i].onplay = (event) => {
-    toggle(event);
+    for (j = 0; j < ctrlVideo.length; ++j) {
+      if (ctrlVideo[j] != event.target) {
+        setVideoState(ctrlVideo[j], false);
+      }
+    }
+    setVideoState(event.target, true);
   };
+
   ctrlVideo[i].onpause = (event) => {
-    toggle(event);
+    setVideoState(event.target, false);
   };
 }
 
-function toggle(event) {
-  const container = event.target.parentElement;
+function setVideoState(video, active) {
+  const container = video.parentElement;
   const button = container.querySelector('.vidbttn');
   const highlight = container.parentElement.querySelector('.highlight__wrapper');
-  container.classList.toggle("active");
-  highlight.classList.add("active");
-  button.classList.toggle("active");
-  button.classList.toggle("paused");
+
+  if (active) {
+    container.classList.add("active");
+    highlight.classList.add("active");
+    button.classList.add("active");
+  }
+
+  else {
+    video.pause();
+
+    container.classList.remove("active");
+    highlight.classList.remove("active");
+    button.classList.remove("active");
+  }
 }
