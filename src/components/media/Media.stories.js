422adb94ddd334a23e504e918c01ca30f011ee1b
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2022-10-12 16:10:52 -0500

    media component barebones

---
+++ src/components/media/Media.stories.js
@@ -0,0 +1,38 @@
+import UidsMedia from './Media.vue';
+import media_image from '../../assets/images/viewbook/sections/122.jpg'
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Basic/Media',
+  component: UidsMedia,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    // Slots
+    // media: {
+    //   control: { type: 'text' },
+    // },
+
+    // Props
+    link_text: {
+      control: { type: 'text' },
+    },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { UidsMedia },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: `
+      <uids-media />
+  `,
+});
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Default.args = { };
---
+++ src/components/media/Media.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import './media.scss'
+import { computed, useSlots } from "vue";
+import { className } from "../utlity";
+
+const name = 'uids-media'
+const props = defineProps({
+  /**
+   * Text to display in a button.
+   */
+  link_text: {
+    type: String,
+  },
+})
+
+const slots = useSlots();
+
+const classes = computed(() => {
+  let classes = ['media media--circle'];
+
+  return classes;
+});
+
+</script>
+
+<template>
+  <div :class="classes">
+    <img alt="" class="" sizes="(min-width:1310px) 1310px, 100vw" srcset="https://sandbox.prod.drupal.uiowa.edu/sites/sandbox.uiowa.edu/files/2021-03/bg-image-square.jpg 384w,https://sandbox.prod.drupal.uiowa.edu/sites/sandbox.uiowa.edu/files/2021-03/bg-image-square.jpg 768w,https://sandbox.prod.drupal.uiowa.edu/sites/sandbox.uiowa.edu/files/2021-03/bg-image-square.jpg 1024w,https://sandbox.prod.drupal.uiowa.edu/sites/sandbox.uiowa.edu/files/2021-03/bg-image-square.jpg 1312w,https://sandbox.prod.drupal.uiowa.edu/sites/sandbox.uiowa.edu/files/2021-03/bg-image-square.jpg 1920w,https://sandbox.prod.drupal.uiowa.edu/sites/sandbox.uiowa.edu/files/2021-03/bg-image-square.jpg 2592w," src="" typeof="foaf:Image">
+  </div>
+</template>
