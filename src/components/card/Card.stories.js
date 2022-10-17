8ec2db15d24d5fa57a384ff81f54220c0bb81644
Benjamin Speare <benjamin-speare@uiowa.edu>
2022-10-17 09:43:13 -0500

    Added longer text snippet

--- src/components/card/Card.stories.js
+++ src/components/card/Card.stories.js
@@ -167,6 +167,6 @@ PersonCard.args = {
   url: "https://uiowa.edu",
   title: "Herky Hawk",
   subtitle: "University of Iowa Mascot",
-  default: "Short biography goes here.",
+  default: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
   borderless: true,
 }
