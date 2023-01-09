6ad86dbeaaa50136bf03b9e6629b3e69a535ff0e
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-01-09 10:08:18 -0600

    Added sizing and removed more unused pieces.

--- src/components/callout/Callout.stories.js
+++ src/components/callout/Callout.stories.js
@@ -1,23 +1,16 @@
 import UidsCallout from './Callout.vue'
 import Background from '../shared/background'
 
+
 export default {
   title: 'Components/Callout',
   component: UidsCallout,
   argTypes: {
-    url: {
-      control: { type: 'text' },
-    },
-    title: {
-      control: { type: 'text' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
     },
     details: {
-      control: { type: 'text' },
-    },
-    button_label: {
-      control: { type: 'text' },
-    },
-    button_icon: {
       control: { type: 'text' },
     },
     ...Background.argTypes,
@@ -35,12 +28,9 @@ const Template = (args) => ({
   // And then the `args` are bound to your component with `v-bind="args"`
   template: `
     <uids-callout
-      :url="args.url"
       :background="args.background"
-      :title="args.title"
-      :button_label="args.button_label"
-      :button_icon="args.button_icon"
       :details="args.details"
+      :size="args.size"
     >
     <template #details v-if="args.details"><div v-html="args.details" ></div></template>
     <template #title v-if="args.title"><div :class="getClasses" v-html="args.title" ></div></template>
@@ -54,7 +44,8 @@ export const Default = Template.bind({});
 Default.args = {
   url: 'https://uiowa.edu/',
   title: 'Be a Hawkeye',
-  details: 'Iowa is where great stories begin. It\'s time to start yours. Find out how.',
+  details: '<h2 class="headline block__headline headline headline--serif headline--underline block__headline headline--center"> <span class="headline__heading"> Resources </span> </h2> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat ante non efficitur laoreet. Suspendisse laoreet cursus dui, eget vehicula massa.</p>',
   button_label: 'Request Information',
   button_icon: '<i class="fas fa-arrow-right"></i>',
+  size: 'medium',
 };
--- src/components/callout/Callout.vue
+++ src/components/callout/Callout.vue
@@ -1,56 +1,43 @@
 <script setup lang="ts">
 import './callout.scss'
+import { computed, useSlots } from 'vue'
+import { className } from '../utlity'
 import '../background/background.scss'
-import UidsHeadline from '../headline/Headline.vue'
-import { computed, useSlots } from "vue";
-import UidsButton from "../button/Button.vue";
 import Background from "../shared/background";
+import '../background/background.scss'
+
+
 const name = 'uids-callout'
 const props = defineProps({
-  url: {
-    type: String,
-  },
   details: {
     type: String,
   },
-  button_label: {
+  size: {
     type: String,
-  },
-  button_icon: {
-    type: String,
-    default: '',
+    default: 'medium',
+    validator: function (value) {
+      return ['small', 'medium', 'large'].indexOf(value) !== -1;
+    },
   },
   ...Background.props,
 });
-const slots = useSlots();
+
 const classes = computed(() => {
-  let classes = ['callout__wrapper'];
+  let classes = ['callout'];
   Background.addBackgroundClass(classes, props);
+  if (props.size) {
+    classes.push(`callout--${ className(props.size)}`);
+  }
+
   return classes;
 });
+
+
+
 </script>
 
 <template>
   <div :class="classes">
-    <div class="callout__container">
-
-
-      <uids-headline v-if="$slots.title" uppercase="true">
-        <slot name="title"></slot>
-      </uids-headline>
-
-
-      <div class="callout__content" v-if="details" >
-        <slot name="details">{{ details }}</slot>
-      </div>
-
-      <footer class="callout__link" v-if="button_label" >
-        <uids-button :url="url" size="medium">
-          <slot name="button_label">{{ button_label }}</slot>
-          <slot name="button_icon"></slot>
-        </uids-button>
-      </footer>
-
-    </div>
+    <slot name="details">{{ details }}</slot>
   </div>
 </template>
