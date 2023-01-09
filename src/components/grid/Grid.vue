814d49a833d7d4c195d3b432f1d5c7c342aedd51
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2023-01-09 16:41:55 -0600

    Added grid component for checking out cards in a grid.

---
+++ src/components/grid/Grid.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import './grid.scss'
+</script>
+<template>
+  <div class="grid grid--3">
+    <slot name="column_1"></slot>
+    <slot name="column_2"></slot>
+    <slot name="column_3"></slot>
+  </div>
+</template>
