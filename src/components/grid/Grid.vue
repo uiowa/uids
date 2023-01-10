91fd34b72c35538f5a2d8ea591cc6f58e1fec6d6
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-01-10 14:27:46 -0600

    Removed double quotes.

--- src/components/grid/Grid.vue
+++ src/components/grid/Grid.vue
@@ -4,13 +4,13 @@ import './grid.scss'
 <template>
   <div class="grid--threecol--33-34-33">
     <div class="list-container">
-      <div class=""grid__column">
+      <div class="grid__column">
         <slot name="column_1"></slot>
       </div>
-      <div class=""grid__column">
+      <div class="grid__column">
         <slot name="column_2"></slot>
       </div>
-      <div class=""grid__column">
+      <div class="grid__column">
         <slot name="column_3"></slot>
       </div>
     </div>
