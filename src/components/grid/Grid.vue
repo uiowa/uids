a85bbaa45d2f217a06b2f401abee2462b2d4b275
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-01-10 14:58:46 -0600

    Added v-if for columns.

--- src/components/grid/Grid.vue
+++ src/components/grid/Grid.vue
@@ -4,13 +4,22 @@ import './grid.scss'
 <template>
   <div class="grid--threecol--33-34-33">
     <div class="list-container">
-      <div class="grid__column">
+      <div
+        v-if="$slots.column_1"
+        class="grid__column"
+      >
         <slot name="column_1"></slot>
       </div>
-      <div class="grid__column">
+      <div
+        v-if="$slots.column_2"
+        class="grid__column"
+      >
         <slot name="column_2"></slot>
       </div>
-      <div class="grid__column">
+      <div
+        v-if="$slots.column_3"
+        class="grid__column"
+      >
         <slot name="column_3"></slot>
       </div>
     </div>
