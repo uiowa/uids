63b239646385b49d5ea7e99283f821ea392ed88a
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-01-10 13:18:54 -0600

    Updated grid to use current grid classes.

--- src/components/grid/Grid.vue
+++ src/components/grid/Grid.vue
@@ -2,9 +2,11 @@
 import './grid.scss'
 </script>
 <template>
-  <div class="grid grid--3">
-    <slot name="column_1"></slot>
-    <slot name="column_2"></slot>
-    <slot name="column_3"></slot>
+  <div class="grid--threecol--33-34-33">
+    <div class="list-container">
+      <slot name="column_1"></slot>
+      <slot name="column_2"></slot>
+      <slot name="column_3"></slot>
+    </div>
   </div>
 </template>
