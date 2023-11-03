ed6497df6ea82a1714844bd1f7a189ca7363a7db
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2023-11-03 09:44:33 -0500

    Alphabetize.

--- src/components/cta/cta.stories.js
+++ src/components/cta/cta.stories.js
@@ -1,26 +1,119 @@
-import UidsStub from "../../components/stub/Stub.vue";
+import UidsCta from './Cta.vue';
+import UidsHeadline from '../headline/Headline.vue';
+import Background from "../shared/background";
 
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
-  title: 'Not implemented/CTA',
-  // https://github.com/storybookjs/storybook/issues/14442#issuecomment-1089165153
-  parameters: { options: { showPanel: false } },
-}
+  title: 'Components/CTA',
+  component: UidsCta,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    // Props
+    headline_style: {
+      name: 'Title style',
+      options: ['uppercase', 'serif',  ''],
+      control: {
+        type: 'select',
+        labels: {
+          'uppercase': 'Antonio (default)',
+          'serif': 'Zilla Slab',
+          '': 'Roboto',
+        },
+      },
+      table: {
+        category: 'Display options',
+      },
+    },
+    orientation: {
+      name: 'Orientation',
+      options: ['', 'left', 'left-right', 'center'],
+      control: {
+        type: 'select',
+        labels: {
+          '': 'Centered (default)',
+          'left': 'Left aligned',
+          'left-right': 'Left aligned with button on the right',
+          'center': 'Center',
+        },
+      },
+      table: {
+        category: 'Display options',
+      },
+    },
+    url: {
+      control: { type: 'text' },
+    },
+    title: {
+      control: { type: 'text' },
+    },
+    details: {
+      control: { type: 'text' },
+    },
+    button_label: {
+      control: { type: 'text' },
+    },
+    button_icon: {
+      control: { type: 'text' },
+    },
+    ...Background.argTypes,
+  },
+};
 
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args) => ({
   // Components used in your story `template` are defined in the `components` object
-  components: { UidsStub },
+  components: { UidsCta },
   // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return { args };
   },
   // And then the `args` are bound to your component with `v-bind="args"`
   template: `
-    <uids-stub
-      path="cta--default.html"
-      title="CTA"
+    <uids-cta
+      :url="args.url"
+      :background="args.background"
+      :title="args.title"
+      :button_label="args.button_label"
+      :button_icon="args.button_icon"
+      :details="args.details"
+      :headline_style="args.headline_style"
+      :orientation="args.orientation"
     >
-    </uids-stub>
+    <template #details v-if="args.details"><div v-html="args.details" ></div></template>
+    <template #title v-if="args.title"><div :class="getClasses" v-html="args.title" ></div></template>
+    <template #button_icon v-if="args.button_icon"><span v-html="args.button_icon" ></span></template>
+    </uids-cta>
   `,
 });
 
-export const CTA = Template.bind({})
+export const Centered = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Centered.args = {
+  url: 'https://uiowa.edu/',
+  title: 'Be a Hawkeye',
+  details: '<p>Iowa is where great stories begin. It\'s time to start yours. Find out how.</p>',
+  button_label: 'Request Information',
+  button_icon: '<i class="fas fa-arrow-right"></i>',
+  headline_style: 'uppercase',
+  background: 'gray',
+  orientation: '',
+};
+
+export const CenteredGrid = Template.bind({});
+CenteredGrid.args = {
+  ...Centered.args,
+  orientation: 'center',
+}
+
+export const LeftAligned = Template.bind({});
+LeftAligned.args = {
+  ...Centered.args,
+  orientation: 'left',
+}
+
+export const LeftAlignedRightAlignedButton = Template.bind({});
+LeftAlignedRightAlignedButton.args = {
+  ...Centered.args,
+  orientation: 'left-right',
+}
