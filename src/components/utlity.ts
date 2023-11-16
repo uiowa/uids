9368f8d6405eacec55d99a1f56ca9a62603b1156
Marie Wakeman <elsie-wakeman@uiowa.edu>
2023-11-16 10:17:34 -0600

    Updated linked location of 3.x components to last 3.x release.

--- src/components/utlity.ts
+++ src/components/utlity.ts
@@ -2,4 +2,4 @@ export function className(text: string): string {
   return text.replace(/_/g, '-');
 }
 
-export  const V3_BASE_URL = 'https://uids.brand.uiowa.edu/';
+export  const V3_BASE_URL = 'https://uids.brand.uiowa.edu/docs/v3.6.21/';
