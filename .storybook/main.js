5e776b6305b388d32badb35182f77322618ab4b0
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-12-01 14:52:38 -0600

    Started storybook 7 migration.

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,25 +3,27 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    {
-      name: "@storybook/addon-essentials",
-      options: {
-        actions: false,
-      },
+
+  "addons": ["@storybook/addon-links", {
+    name: "@storybook/addon-essentials",
+    options: {
+      actions: false,
     },
-    "@storybook/preset-scss",
-    "@whitespace/storybook-addon-html",
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  }, "@storybook/preset-scss", "@whitespace/storybook-addon-html", "@storybook/addon-mdx-gfm"],
+
+  "framework": {
+    name: "@storybook/vue3-vite",
+    options: {}
   },
+
   async viteFinal(config, { configType }) {
     config.base = process.env.BASE_URL || config.base;
 
     // return the customized config
     return config;
   },
+  
+  docs: {
+    autodocs: true
+  }
 }
