8f24bae87b79daba8ae6e239912ef89ef075f70b
bspeare <bspeare@users.noreply.github.com>
2024-01-31 22:17:13 +0000

    Deploying to gh-pages from @ uiowa/uids@e14572facdbf2a68c68a5c750cc0c14ca32e082a 🚀

---
+++ docs/v4.0.0-alpha3/assets/Logo.stories-845bc28e.js
@@ -0,0 +1,15 @@
+import{_ as n}from"./Logo-c27202e4.js";import"./vue.esm-bundler-f09df2bf.js";const d={title:"Components/Branding/Logo",parameters:{docs:{source:{code:null}}},component:n,tags:["autodocs"]},a=r=>({components:{UidsLogo:n},setup(){return{args:r}},template:`
+    <uids-logo></uids-logo>
+  `}),o=a.bind({});var e,s,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  components: {
+    UidsLogo
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`
+    <uids-logo></uids-logo>
+  \`
+})`,...(t=(s=o.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const m=["Default"];export{o as Default,m as __namedExportsOrder,d as default};
