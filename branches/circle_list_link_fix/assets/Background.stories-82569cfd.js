10baa55fdbadab7c2c4753ec019cea1e85fa620e
bspeare <bspeare@users.noreply.github.com>
2024-06-11 21:15:57 +0000

    Deploying to gh-pages from @ uiowa/uids@fc950c748074a2fcae2dceff4d5a6da35687588b 🚀

---
+++ branches/circle_list_link_fix/assets/Background.stories-82569cfd.js
@@ -0,0 +1,26 @@
+import{_ as r}from"./Stub-86bed54b.js";import"./vue.esm-bundler-f09df2bf.js";import"./utlity-a0fec18e.js";import"./Button-d429cc7b.js";import"./Headline-e6cd25c2.js";const i={title:"Not implemented/Background",parameters:{docs:{source:{code:null}},options:{showPanel:!1}}},a=s=>({components:{UidsStub:r},setup(){return{args:s}},template:`
+    <uids-stub
+      path="background.html"
+      title="Background"
+    >
+    </uids-stub>
+  `}),t=a.bind({});var e,n,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  // Components used in your story \`template\` are defined in the \`components\` object
+  components: {
+    UidsStub
+  },
+  // The story's \`args\` need to be mapped into the template through the \`setup()\` method
+  setup() {
+    return {
+      args
+    };
+  },
+  // And then the \`args\` are bound to your component with \`v-bind="args"\`
+  template: \`
+    <uids-stub
+      path="background.html"
+      title="Background"
+    >
+    </uids-stub>
+  \`
+})`,...(o=(n=t.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const l=["Background"];export{t as Background,l as __namedExportsOrder,i as default};
