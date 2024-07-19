2e98d3ee0dab9e4dc19cf979aa3a958fcb6ecef0
bspeare <bspeare@users.noreply.github.com>
2024-07-19 19:16:56 +0000

    Deploying to gh-pages from @ uiowa/uids@ebb23e9f2c89d13658a696c0ec29c02bd91017c2 🚀

---
+++ branches/4x_blockquote/assets/BrandBarDocs-a19f7d55.js
@@ -0,0 +1,22 @@
+import{j as s}from"./jsx-runtime-6eef64cc.js";import{M as o,T as a,P as c,d,e as h}from"./index-040419d7.js";import{v as e}from"./package-68a66de7.js";import{u as r}from"./index-dc1d5b46.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";import"./iframe-e833f66e.js";import"../sb-preview/runtime.js";import"./index-11d98b33.js";import"./index-38ad35d1.js";import"./index-356e4a49.js";function i(t){const n=Object.assign({h2:"h2",p:"p",a:"a",strong:"strong",ul:"ul",li:"li",hr:"hr"},r(),t.components);return s.jsxs(s.Fragment,{children:[s.jsx(o,{isTemplate:!0}),`
+`,s.jsx(a,{}),`
+`,s.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,s.jsxs(n.p,{children:["Make sure you are familiar with the ",s.jsx(n.a,{href:"../?path=/docs/introduction#getting-started",children:s.jsx(n.strong,{children:"Getting Started"})})," guide first."]}),`
+`,s.jsx(n.p,{children:"Add the following files to your project:"}),`
+`,s.jsxs(n.ul,{children:[`
+`,s.jsxs(n.li,{children:[`
+`,s.jsx("strong",{children:s.jsx("a",{href:`https://github.com/uiowa/uids/tree/gh-pages/docs/v${e}/dist/css/components/logo.css`,children:"logo.css"})}),`
+`]}),`
+`,s.jsxs(n.li,{children:[`
+`,s.jsx("strong",{children:s.jsx("a",{href:`https://github.com/uiowa/uids/tree/gh-pages/docs/v${e}/dist/css/components/brand-bar.css`,children:"brand-bar.css"})}),`
+`]}),`
+`]}),`
+`,s.jsx(n.p,{children:"Refer to the HTML tab for example HTML."}),`
+`,s.jsx(c,{withSource:"none"}),`
+`,s.jsx(n.h2,{id:"inputs",children:"Inputs"}),`
+`,s.jsx(n.p,{children:"The component accepts the following inputs (props):"}),`
+`,s.jsx(d,{}),`
+`,s.jsx(n.hr,{}),`
+`,s.jsx(n.h2,{id:"additional-variations",children:"Additional variations"}),`
+`,s.jsx(n.p,{children:"Listed below are additional variations of the component."}),`
+`,s.jsx(h,{})]})}function v(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?s.jsx(n,Object.assign({},t,{children:s.jsx(i,t)})):i(t)}export{v as default};
---
+++ branches/4x_blockquote/assets/package-68a66de7.js
@@ -0,0 +1,1 @@
+const a="4.0.0-alpha5";export{a as v};
