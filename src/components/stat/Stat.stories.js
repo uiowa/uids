6db5d298aa61ca854f956bfb74a420945c69f710
Benjamin Speare <benjamin-speare@uiowa.edu>
2024-11-08 15:35:19 -0600

    Added stat component.

--- src/components/stat/Stat.stories.js
+++ src/components/stat/Stat.stories.js
@@ -1,36 +1,62 @@
-import UidsStub from "../../components/stub/Stub.vue";
+import UidsStat from './Stat.vue';
+import Background from "../shared/background";
 
 export default {
-  title: 'Not implemented/Stat',
-  tags: ['!autodocs'],
-  // https://github.com/storybookjs/storybook/issues/14442#issuecomment-1089165153
-  parameters: {
-    docs: {
-      source: {
-        code: null,
-      },
+  title: 'Components/Stat',
+  component: UidsStat,
+  tags: ['autodocs'],
+  argTypes: {
+    display: {
+      control: 'select',
+      options: ['default', 'horizontal'],
+      description: 'Display style of the stat',
     },
-    options: {
-      showPanel: false,
+    stat_title: {
+      control: 'text',
+      description: 'The main statistic number/text',
     },
+    stat_summary: {
+      control: 'text',
+      description: 'Description of the statistic',
+    },
+    stat_content: {
+      control: 'text',
+      description: 'Additional content below the summary',
+    },
+    stat_prefix: {
+      control: 'text',
+      description: 'Text/symbol to display before the title',
+    },
+    stat_suffix: {
+      control: 'text',
+      description: 'Text/symbol to display after the title',
+    },
+    ...Background.argTypes,
   },
-}
+};
 
 const Template = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
-  components: { UidsStub },
-  // The story's `args` need to be mapped into the template through the `setup()` method
+  components: { UidsStat },
   setup() {
     return { args };
   },
-  // And then the `args` are bound to your component with `v-bind="args"`
-  template: `
-    <uids-stub
-      path="stat--default.html"
-      title="Stat"
-    >
-    </uids-stub>
-  `,
+  template: '<uids-stat v-bind="args" />',
 });
 
-export const Stat = Template.bind({})
+export const Default = Template.bind({});
+Default.args = {
+  display: 'default',
+  stat_title: '15:1',
+  stat_summary: 'student-to-faculty ratio',
+  stat_content: 'Among the top 2% of universities worldwide.',
+  stat_prefix: '',
+  stat_suffix: '+',
+};
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+  display: 'horizontal',
+  stat_title: '15:1',
+  stat_summary: 'student-to-faculty<br /> ratio',
+  stat_content: 'Among the top 2% of universities worldwide.',
+};
---
+++ src/components/stat/Stat.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import '../../scss/components/stat.scss'
+import '../../scss/components/_background.scss'
+import { computed } from "vue";
+import Background from "../shared/background";
+
+const name = 'uids-stat';
+const props = defineProps({
+  stat_title: {
+    type: String,
+    default: '',
+  },
+  stat_summary: {
+    type: String,
+    default: '',
+  },
+  stat_content: {
+    type: String,
+    default: '',
+  },
+  stat_prefix: {
+    type: String,
+    default: '',
+  },
+  stat_suffix: {
+    type: String,
+    default: '',
+  },
+  display: {
+    type: String,
+    default: 'default',
+    validator: (value: string) => ['default', 'horizontal'].includes(value)
+  },
+  ...Background.props,
+});
+
+const classes = computed(() => {
+  let classes = [];
+  Background.addBackgroundClass(classes, props);
+
+  if (props.display === 'horizontal') {
+    classes.push('stat--horizontal', 'stat--transform', 'stat__grid');
+  } else {
+    classes.push('element--flex-center', 'stat--transform');
+  }
+
+  return classes;
+});
+</script>
+
+<template>
+  <div :class="classes">
+    <div v-if="stat_title">
+      <h2 class="stat__title">
+        <span v-if="stat_prefix" class="headline__prefix">{{ stat_prefix }}</span>
+        {{ stat_title }}
+        <span v-if="stat_suffix" class="headline__suffix">{{ stat_suffix }}</span>
+      </h2>
+    </div>
+
+    <p v-if="stat_summary" class="stat__description" v-html="stat_summary"></p>
+
+    <p v-if="stat_content" class="stat__content" v-html="stat_content"></p>
+  </div>
+</template>
