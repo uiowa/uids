d2fd4357c370dfdb14c749c9362bd189c9ae0cdc
bspeare <bspeare@users.noreply.github.com>
2024-11-15 20:18:21 +0000

    Deploying to gh-pages from @ uiowa/uids@5cfd553a1f94d7479aae0b6392038afcf31dc4e0 🚀

---
+++ branches/4x_footer/assets/index-KHfEjFFi.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-BO1UisG0.js";import"./index-pvMz8cLs.js";import"./iframe-CdeQuUQv.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
