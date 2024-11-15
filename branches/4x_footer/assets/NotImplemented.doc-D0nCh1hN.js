3f146c4c30b905fc2907347c9ef2072872913130
bspeare <bspeare@users.noreply.github.com>
2024-11-15 20:50:37 +0000

    Deploying to gh-pages from @ uiowa/uids@9901392a93ec32f74abc383cf8d1fa62a14d9375 🚀

---
+++ branches/4x_footer/assets/NotImplemented.doc-D0nCh1hN.js
@@ -0,0 +1,10 @@
+import{ae as o,af as i,ag as t}from"./index-CicnS-cN.js";import{u as e}from"./index-DBsL-iDZ.js";import{Accordion as p}from"./Accordion.stories-C1fyinsR.js";import{Badge as s}from"./Badge.stories-p9OCFbNR.js";import{Banner as a}from"./Banner.stories-CzF8dxOH.js";import{Slider as f}from"./Slider.stories-Cclp6TSh.js";import{Stat as x}from"./Stat.stories-DbX-3Cwj.js";import{Table as c}from"./Table.stories-C-3CGNdQ.js";import{Tabs as d}from"./Tabs.stories-DEjMybki.js";import"./iframe-DHeVYJKi.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";import"./Stub.vue_vue_type_script_setup_true_lang-WzhSnLmx.js";import"./vue.esm-bundler-B7frGVmg.js";import"./utlity-CAV5GFHh.js";import"./Button.vue_vue_type_script_setup_true_lang-BtFSlIHd.js";import"./Headline.vue_vue_type_script_setup_true_lang-Ccvxo8_6.js";import"./_background-CmPCPj8O.js";function r(n){const m={h1:"h1",p:"p",...e(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Not Implemented"}),`
+`,o.jsx(m.h1,{id:"not-implemented",children:"Not implemented"}),`
+`,o.jsx(m.p,{children:"The following components are not implemented in UIDS 4 as of this time."}),`
+`,o.jsx(t,{of:p}),`
+`,o.jsx(t,{of:s}),`
+`,o.jsx(t,{of:a}),`
+`,o.jsx(t,{of:f}),`
+`,o.jsx(t,{of:x}),`
+`,o.jsx(t,{of:c}),`
+`,o.jsx(t,{of:d})]})}function E(n={}){const{wrapper:m}={...e(),...n.components};return m?o.jsx(m,{...n,children:o.jsx(r,{...n})}):r(n)}export{E as default};
