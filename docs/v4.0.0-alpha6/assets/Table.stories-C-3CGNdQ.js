ea5a0465e5411c779bb1b47498ffe7c47c6cb637
bspeare <bspeare@users.noreply.github.com>
2024-11-18 15:34:17 +0000

    Deploying to gh-pages from @ uiowa/uids@8de48abf26019d71e98a1dd7eb4cad5b078b2334 🚀

---
+++ docs/v4.0.0-alpha6/assets/Table.stories-C-3CGNdQ.js
@@ -0,0 +1,26 @@
+import{_ as a}from"./Stub.vue_vue_type_script_setup_true_lang-WzhSnLmx.js";import"./vue.esm-bundler-B7frGVmg.js";import"./utlity-CAV5GFHh.js";import"./Button.vue_vue_type_script_setup_true_lang-BtFSlIHd.js";import"./Headline.vue_vue_type_script_setup_true_lang-Ccvxo8_6.js";const l={title:"Not implemented/Table",tags:["!autodocs"],parameters:{docs:{source:{code:null}},options:{showPanel:!1}}},r=o=>({components:{UidsStub:a},setup(){return{args:o}},template:`
+    <uids-stub
+      path="tables.html"
+      title="Tables"
+    >
+    </uids-stub>
+  `}),t=r.bind({});var e,s,n;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  // Components used in your story \`template\` are defined in the \`components\` object
+  components: {
+    UidsStub
+  },
+  // The story's \`args\` need to be mapped into the template through the \`setup()\` method
+  setup() {
+    return {
+      args
+    };
+  },
+  // And then the \`args\` are bound to your component with \`v-bind="args"\`
+  template: \`
+    <uids-stub
+      path="tables.html"
+      title="Tables"
+    >
+    </uids-stub>
+  \`
+})`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const c=["Table"];export{t as Table,c as __namedExportsOrder,l as default};
