053f2981859d188dda5751a3fc9ed1aa641bd5ee
bspeare <bspeare@users.noreply.github.com>
2024-12-17 16:16:26 +0000

    Deploying to gh-pages from @ uiowa/uids@ac37be64ac0a16f7daf74385efe4a378b647793d 🚀

---
+++ branches/banner-component/assets/DocumentationTemplate-ChjpTFEs.js
@@ -0,0 +1,10 @@
+import{ae as n,af as e,ah as a,ai as r,aj as p,ak as m}from"./index-BHuwNEyP.js";import{u as i}from"./index-DT3t2v9e.js";import"./iframe-DvGJAWxQ.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";function s(o){const t={h2:"h2",hr:"hr",p:"p",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{isTemplate:!0}),`
+`,n.jsx(a,{}),`
+`,n.jsx(r,{}),`
+`,n.jsx(t.h2,{id:"inputs",children:"Inputs"}),`
+`,n.jsx(t.p,{children:"The component accepts the following inputs (props):"}),`
+`,n.jsx(p,{}),`
+`,n.jsx(t.hr,{}),`
+`,n.jsx(t.h2,{id:"additional-variations",children:"Additional variations"}),`
+`,n.jsx(t.p,{children:"Listed below are additional variations of the component."}),`
+`,n.jsx(m,{})]})}function f(o={}){const{wrapper:t}={...i(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(s,{...o})}):s(o)}export{f as default};
---
+++ branches/banner-component/assets/index-D58m1eei.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-DT3t2v9e.js";import"./index-BHuwNEyP.js";import"./iframe-DvGJAWxQ.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
