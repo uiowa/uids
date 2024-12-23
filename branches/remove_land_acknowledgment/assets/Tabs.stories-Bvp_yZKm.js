18d9a8c92305d87f6b19ba16e6ed4f6339743f4f
pyrello <pyrello@users.noreply.github.com>
2024-12-23 20:58:11 +0000

    Deploying to gh-pages from @ uiowa/uids@37b3491362fb5a11c481b5ba298d4292c7bd0a36 🚀

---
+++ branches/remove_land_acknowledgment/assets/Tabs.stories-Bvp_yZKm.js
@@ -0,0 +1,26 @@
+import{_ as a}from"./Stub.vue_vue_type_script_setup_true_lang-2TpcIRzT.js";import"./vue.esm-bundler-BcQIfK0R.js";import"./utlity-CAV5GFHh.js";import"./Button.vue_vue_type_script_setup_true_lang-C0a49Wt5.js";import"./Headline.vue_vue_type_script_setup_true_lang-BPeQ6PvJ.js";const c={title:"Not implemented/Tabs",tags:["!autodocs"],parameters:{docs:{source:{code:null}},options:{showPanel:!1}}},r=o=>({components:{UidsStub:a},setup(){return{args:o}},template:`
+    <uids-stub
+      path="tabs.html"
+      title="Tabs"
+    >
+    </uids-stub>
+  `}),t=r.bind({});var e,s,n;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  // Components used in your story \`template\` are defined in the \`components\` object
+  components: {
+    UidsStub
+  },
+  // The story's \`args\` need to be mapped into the template through the \`setup()\` method
+  setup() {
+    return {
+      args
+    };
+  },
+  // And then the \`args\` are bound to your component with \`v-bind="args"\`
+  template: \`
+    <uids-stub
+      path="tabs.html"
+      title="Tabs"
+    >
+    </uids-stub>
+  \`
+})`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const b=["Tabs"];export{t as Tabs,b as __namedExportsOrder,c as default};
---
+++ branches/remove_land_acknowledgment/assets/index-DCcf9akh.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-CLtkoaT-.js";import"./index-C83TwgJl.js";import"./iframe-BB81OqXu.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
