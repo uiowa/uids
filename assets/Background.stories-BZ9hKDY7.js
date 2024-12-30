482f9b8052ef58a6db69743b6c2b13a4d67b6b08
james917 <james-leone@uiowa.edu>
2024-12-30 17:18:08 +0000

    Deploying to gh-pages from @ uiowa/uids@e0fc689c26bc0275b36ace973d9ab1754b64b57f 🚀

---
+++ assets/Background.stories-BZ9hKDY7.js
@@ -0,0 +1,26 @@
+import{_ as r}from"./Stub.vue_vue_type_script_setup_true_lang-2TpcIRzT.js";import"./vue.esm-bundler-BcQIfK0R.js";import"./utlity-CAV5GFHh.js";import"./Button.vue_vue_type_script_setup_true_lang-C0a49Wt5.js";import"./Headline.vue_vue_type_script_setup_true_lang-BPeQ6PvJ.js";const i={title:"Not implemented/Background",tags:["!autodocs"],parameters:{docs:{source:{code:null}},options:{showPanel:!1}}},a=s=>({components:{UidsStub:r},setup(){return{args:s}},template:`
+    <uids-stub
+      path="background.html"
+      title="Background"
+    >
+    </uids-stub>
+  `}),t=a.bind({});var e,n,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  // Components used in your story \`template\` are defined in the \`components\` object
+  components: {
+    UidsStub
+  },
+  // The story's \`args\` need to be mapped into the template through the \`setup()\` method
+  setup() {
+    return {
+      args
+    };
+  },
+  // And then the \`args\` are bound to your component with \`v-bind="args"\`
+  template: \`
+    <uids-stub
+      path="background.html"
+      title="Background"
+    >
+    </uids-stub>
+  \`
+})`,...(o=(n=t.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const l=["Background"];export{t as Background,l as __namedExportsOrder,i as default};
---
+++ docs/v4.0.0-alpha8/assets/index-SakKp1DN.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-CSjwdkpp.js";import"./index-2y95r4Fd.js";import"./iframe-BQpaiRHs.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
