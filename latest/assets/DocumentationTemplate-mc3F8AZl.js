adab77321c4f5eecf3e09c9905f780f6873f8f94
bspeare <bspeare@users.noreply.github.com>
2025-01-23 18:44:00 +0000

    Deploying to gh-pages from @ uiowa/uids@1b554388f73ba5777ca774b0b3e092dbbe709f51 🚀

--- latest/assets/DocumentationTemplate-mc3F8AZl.js
+++ latest/assets/DocumentationTemplate-mc3F8AZl.js
@@ -1,4 +1,4 @@
-import{ae as n,af as e,ah as a,ai as r,aj as p,al as m}from"./index-COyFFlnF.js";import{u as i}from"./index-B3aUjo35.js";import"./iframe-B08S7Ky6.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";function s(o){const t={h2:"h2",hr:"hr",p:"p",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{isTemplate:!0}),`
+import{ae as n,af as e,ah as a,ai as r,aj as p,al as m}from"./index-CEFpp6G7.js";import{u as i}from"./index-BNzNB8rW.js";import"./iframe-DaL3mCWk.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";function s(o){const t={h2:"h2",hr:"hr",p:"p",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{isTemplate:!0}),`
 `,n.jsx(a,{}),`
 `,n.jsx(r,{}),`
 `,n.jsx(t.h2,{id:"inputs",children:"Inputs"}),`
