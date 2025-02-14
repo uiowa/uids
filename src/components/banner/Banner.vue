454d308b2bced115e13fa9d5a96f21ce4146aedf
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2025-02-14 17:16:44 -0600

    Made previous gradient-left style rules the base case when gradient is being applied. Converted gradient-top class rules into using top vertical alignment class instead. Updated vue template to reflect the removal of those CSS classes.

--- src/components/banner/Banner.vue
+++ src/components/banner/Banner.vue
@@ -110,20 +110,15 @@ const classes = computed(() => {
     classes.push(`banner--vertical-${props.vertical_alignment}`);
   }
 
+  // @todo Remove this section when banner SCSS changes are complete.
   // Only apply a gradient if we don't have a background.
   if (props.background === '') {
 
-    if (props.vertical_alignment === 'top') {
-      classes.push('banner--gradient-top');
-    }
-    else if (
+    if (
       props.vertical_alignment === 'bottom' ||
       props.horizontal_alignment === 'center'
     ) {
       classes.push('banner--gradient-bottom');
-    }
-    else {
-      classes.push('banner--gradient-left');
     }
   }
 
