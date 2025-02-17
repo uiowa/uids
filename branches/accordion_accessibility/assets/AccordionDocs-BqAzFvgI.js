c8008a6eb7c95408f1d5edcf1d54bc153211ab59
GaryRidgway <GaryRidgway@users.noreply.github.com>
2025-02-17 19:54:42 +0000

    Deploying to gh-pages from @ uiowa/uids@d6017755c93458c60d106356211a04435d55602a 🚀

---
+++ branches/accordion_accessibility/assets/AccordionDocs-BqAzFvgI.js
@@ -0,0 +1,23 @@
+import{ae as n,af as o,ah as a,ai as c,aj as d,ak as h,al as l}from"./index-Ckf5XwTV.js";import{u as r}from"./index-RQuvXiZV.js";import{v as e}from"./package-Chrc7M3U.js";import"./iframe-Bu8sx9ix.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";function i(t){const s={a:"a",h2:"h2",hr:"hr",li:"li",p:"p",strong:"strong",ul:"ul",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(o,{isTemplate:!0}),`
+`,n.jsx(a,{}),`
+`,n.jsx(s.h2,{id:"usage",children:"Usage"}),`
+`,n.jsxs(s.p,{children:["Make sure you are familiar with the ",n.jsx(s.a,{href:"../?path=/docs/introduction#getting-started",children:n.jsx(s.strong,{children:"Getting Started"})})," guide first."]}),`
+`,n.jsx(s.p,{children:"Add the following files to your project:"}),`
+`,n.jsxs(s.ul,{children:[`
+`,n.jsxs(s.li,{children:[`
+`,n.jsx("strong",{children:n.jsx("a",{href:`https://github.com/uiowa/uids/tree/gh-pages/docs/v${e}/dist/css/components/logo.css`,children:"logo.css"})}),`
+`]}),`
+`,n.jsxs(s.li,{children:[`
+`,n.jsx("strong",{children:n.jsx("a",{href:`https://github.com/uiowa/uids/tree/gh-pages/docs/v${e}/dist/css/components/brand-bar.css`,children:"brand-bar.css"})}),`
+`]}),`
+`]}),`
+`,n.jsx(s.p,{children:"Refer to the HTML tab for example HTML."}),`
+`,n.jsx(c,{withSource:"none"}),`
+`,n.jsx(s.h2,{id:"inputs",children:"Inputs"}),`
+`,n.jsx(s.p,{children:"The component accepts the following inputs (props):"}),`
+`,n.jsx(d,{}),`
+`,n.jsx(h,{}),`
+`,n.jsx(s.hr,{}),`
+`,n.jsx(s.h2,{id:"additional-variations",children:"Additional variations"}),`
+`,n.jsx(s.p,{children:"Listed below are additional variations of the component."}),`
+`,n.jsx(l,{})]})}function w(t={}){const{wrapper:s}={...r(),...t.components};return s?n.jsx(s,{...t,children:n.jsx(i,{...t})}):i(t)}export{w as default};
---
+++ branches/accordion_accessibility/assets/index-BVUCGYV_.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-RQuvXiZV.js";import"./index-Ckf5XwTV.js";import"./iframe-Bu8sx9ix.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
