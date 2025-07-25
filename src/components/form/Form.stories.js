9716ef4d94f66a3b900edf93869c0fc0ff6f0d1e
Benjamin Speare <benjamin-speare@uiowa.edu>
2025-07-25 08:26:46 -0500

    Added some flexbox to the form.

--- src/components/form/Form.stories.js
+++ src/components/form/Form.stories.js
@@ -1,4 +1,4 @@
-import UidsForm from './Form.vue'
+import UidsForm from './Form.vue';
 import FormDocs from './FormDocs.mdx';
 
 export default {
@@ -6,15 +6,14 @@ export default {
   parameters: {
     docs: {
       source: {
-        code: null,
+        code: null
       },
-      page: FormDocs,
-    },
+      page: FormDocs
+    }
   },
   component: UidsForm,
   tags: ['autodocs'],
-  argTypes: {
-  },
+  argTypes: {}
 };
 
 const BasicFormTemplate = (args) => ({
@@ -22,7 +21,7 @@ const BasicFormTemplate = (args) => ({
   setup() {
     return { args };
   },
-  template: '<uids-form v-bind="args" />',
+  template: '<uids-form v-bind="args" />'
 });
 
 export const Form = BasicFormTemplate.bind({});
@@ -45,68 +44,83 @@ const MiscTemplate = (args) => ({
                 <legend class="visually-hidden">Select one or more genres</legend>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-childrens--Y2idzqPWjuo" name="field_event_genre_value[Childrens]" value="Childrens" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-childrens--Y2idzqPWjuo"
+                         name="field_event_genre_value[Childrens]" value="Childrens" class="form-checkbox">
                   <label for="edit-field-event-genre-value-childrens--Y2idzqPWjuo" class="option">Children's</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-essay--qn92WZNFxVQ" name="field_event_genre_value[Essay]" value="Essay" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-essay--qn92WZNFxVQ"
+                         name="field_event_genre_value[Essay]" value="Essay" class="form-checkbox">
                   <label for="edit-field-event-genre-value-essay--qn92WZNFxVQ" class="option">Essay</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-fantasy--qsdFbcezUn0" name="field_event_genre_value[Fantasy]" value="Fantasy" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-fantasy--qsdFbcezUn0"
+                         name="field_event_genre_value[Fantasy]" value="Fantasy" class="form-checkbox">
                   <label for="edit-field-event-genre-value-fantasy--qsdFbcezUn0" class="option">Fantasy</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-fiction--F547iWBi4wg" name="field_event_genre_value[Fiction]" value="Fiction" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-fiction--F547iWBi4wg"
+                         name="field_event_genre_value[Fiction]" value="Fiction" class="form-checkbox">
                   <label for="edit-field-event-genre-value-fiction--F547iWBi4wg" class="option">Fiction</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-hybridforms--NgTo0EtnD_A" name="field_event_genre_value[HybridForms]" value="HybridForms" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-hybridforms--NgTo0EtnD_A"
+                         name="field_event_genre_value[HybridForms]" value="HybridForms" class="form-checkbox">
                   <label for="edit-field-event-genre-value-hybridforms--NgTo0EtnD_A" class="option">Hybrid Forms</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-memoir--HVUn8CLgzqg" name="field_event_genre_value[Memoir]" value="Memoir" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-memoir--HVUn8CLgzqg"
+                         name="field_event_genre_value[Memoir]" value="Memoir" class="form-checkbox">
                   <label for="edit-field-event-genre-value-memoir--HVUn8CLgzqg" class="option">Memoir</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-middlegrade--ueW_97yE7nY" name="field_event_genre_value[MiddleGrade]" value="MiddleGrade" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-middlegrade--ueW_97yE7nY"
+                         name="field_event_genre_value[MiddleGrade]" value="MiddleGrade" class="form-checkbox">
                   <label for="edit-field-event-genre-value-middlegrade--ueW_97yE7nY" class="option">Middle Grade</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-nonfiction--RO3rViX_wJI" name="field_event_genre_value[Nonfiction]" value="Nonfiction" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-nonfiction--RO3rViX_wJI"
+                         name="field_event_genre_value[Nonfiction]" value="Nonfiction" class="form-checkbox">
                   <label for="edit-field-event-genre-value-nonfiction--RO3rViX_wJI" class="option">Nonfiction</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-novel--nY9Utkyye3w" name="field_event_genre_value[Novel]" value="Novel" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-novel--nY9Utkyye3w"
+                         name="field_event_genre_value[Novel]" value="Novel" class="form-checkbox">
                   <label for="edit-field-event-genre-value-novel--nY9Utkyye3w" class="option">Novel</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-playwriting--nTE1KzbGVyM" name="field_event_genre_value[Playwriting]" value="Playwriting" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-playwriting--nTE1KzbGVyM"
+                         name="field_event_genre_value[Playwriting]" value="Playwriting" class="form-checkbox">
                   <label for="edit-field-event-genre-value-playwriting--nTE1KzbGVyM" class="option">Playwriting</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-poetry--YJd6UDHH86E" name="field_event_genre_value[Poetry]" value="Poetry" class="form-checkbox">
+                  <input type="checkbox" id="edit-field-event-genre-value-poetry--YJd6UDHH86E"
+                         name="field_event_genre_value[Poetry]" value="Poetry" class="form-checkbox">
                   <label for="edit-field-event-genre-value-poetry--YJd6UDHH86E" class="option">Poetry</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-sciencefiction--s7nRw8QKJ44" name="field_event_genre_value[ScienceFiction]" value="ScienceFiction" class="form-checkbox">
-                  <label for="edit-field-event-genre-value-sciencefiction--s7nRw8QKJ44" class="option">Science Fiction</label>
+                  <input type="checkbox" id="edit-field-event-genre-value-sciencefiction--s7nRw8QKJ44"
+                         name="field_event_genre_value[ScienceFiction]" value="ScienceFiction" class="form-checkbox">
+                  <label for="edit-field-event-genre-value-sciencefiction--s7nRw8QKJ44" class="option">Science
+                    Fiction</label>
                 </div>
 
                 <div class="form-type-checkbox form-item">
-                  <input type="checkbox" id="edit-field-event-genre-value-screenwriting--3SCKNL7gdww" name="field_event_genre_value[Screenwriting]" value="Screenwriting" class="form-checkbox">
-                  <label for="edit-field-event-genre-value-screenwriting--3SCKNL7gdww" class="option">Screenwriting</label>
+                  <input type="checkbox" id="edit-field-event-genre-value-screenwriting--3SCKNL7gdww"
+                         name="field_event_genre_value[Screenwriting]" value="Screenwriting" class="form-checkbox">
+                  <label for="edit-field-event-genre-value-screenwriting--3SCKNL7gdww"
+                         class="option">Screenwriting</label>
                 </div>
               </fieldset>
             </div>
@@ -166,12 +180,14 @@ const MiscTemplate = (args) => ({
               </div>
 
               <div class="form-type-checkbox form-item">
-                <input role="switch" id="academic-preprofessional" type="checkbox" name="academic_group[301]" value="301">
+                <input role="switch" id="academic-preprofessional" type="checkbox" name="academic_group[301]"
+                       value="301">
                 <label for="academic-preprofessional" class="checkbox">Preprofessional Programs</label>
               </div>
 
               <div class="form-type-checkbox form-item">
-                <input role="switch" id="academic-social-sciences" type="checkbox" name="academic_group[291]" value="291">
+                <input role="switch" id="academic-social-sciences" type="checkbox" name="academic_group[291]"
+                       value="291">
                 <label for="academic-social-sciences" class="checkbox">Social Sciences and Policy</label>
               </div>
 
@@ -190,64 +206,67 @@ const MiscTemplate = (args) => ({
           </div>
         </fieldset>
 
-      <div class="form-item">
-        <label for="disabled-field">Disabled</label>
-        <input id="disabled-field" type="text" name="disabled" value="" size="60" maxlength="255" placeholder="This field is disabled" disabled="disabled">
-      </div>
-
-      <div class="form-item">
-        <label for="readonly-field">Readonly</label>
-        <input id="readonly-field" type="text" name="readonly" value="" size="60" maxlength="255" placeholder="Readonly" readonly="readonly">
-      </div>
-
-      <div class="form-item">
-        <label for="edit-textarea">Textarea</label>
-        <textarea id="edit-textarea" name="textarea" rows="5" cols="60" aria-describedby="edit-textarea--description"></textarea>
-        <div id="edit-textarea--description" class="description">Textarea description goes here.</div>
-      </div>
-
-      <fieldset>
-        <legend>
-          <span class="fieldset-legend">Select any additional subjects that you would like to receive information about.</span>
-        </legend>
-        <div>
-          <div class="form-type-checkbox form-type-checkbox--side form-item">
-            <input id="financial-aid-2" type="checkbox" value="FINANCIAL_AID">
-            <label for="financial-aid-2" class="checkbox">Costs & Financial Aid</label>
-          </div>
-          <div class="form-type-checkbox form-type-checkbox--side form-item">
-            <input id="marching-band-2" type="checkbox" value="MARCHING_BAND">
-            <label for="marching-band-2" class="checkbox">Hawkeye Marching Band</label>
-          </div>
-          <div class="form-type-checkbox form-type-checkbox--side form-item">
-            <input id="rotc-air-force-2" type="checkbox" value="ROTC_AIR_FORCE">
-            <label for="rotc-air-force-2" class="checkbox">ROTC - Air Force (Aerospace Studies)</label>
-          </div>
-          <div class="form-type-checkbox form-type-checkbox--side form-item">
-            <input id="rotc-army-2" type="checkbox" value="ROTC_ARMY">
-            <label for="rotc-army-2" class="checkbox">ROTC - Army (Military Science)</label>
-          </div>
-          <div class="form-type-checkbox form-type-checkbox--side form-item">
-            <input id="study-abroad-2" type="checkbox" value="STUDY_ABROAD">
-            <label for="study-abroad-2" class="checkbox">Study Abroad</label>
-          </div>
-          <div class="form-type-checkbox form-type-checkbox--side form-item">
-            <input id="band-2" type="checkbox" value="BAND">
-            <label for="band-2" class="checkbox">University Concert Bands and Jazz Bands</label>
-          </div>
-          <div class="form-type-checkbox form-type-checkbox--side form-item">
-            <input id="honors-2" type="checkbox" value="HONORS">
-            <label for="honors-2" class="checkbox">University Honors Program</label>
-          </div>
-          <div class="form-type-checkbox form-type-checkbox--side form-item">
-            <input id="veteran-services-2" type="checkbox" value="VETERAN_SERVICES">
-            <label for="veteran-services-2" class="checkbox">Veteran & Military Connected Student Services</label>
-          </div>
+        <div class="form-item">
+          <label for="disabled-field">Disabled</label>
+          <input id="disabled-field" type="text" name="disabled" value="" size="60" maxlength="255"
+                 placeholder="This field is disabled" disabled="disabled">
         </div>
-      </fieldset>
+
+        <div class="form-item">
+          <label for="readonly-field">Readonly</label>
+          <input id="readonly-field" type="text" name="readonly" value="" size="60" maxlength="255"
+                 placeholder="Readonly" readonly="readonly">
+        </div>
+
+        <div class="form-item">
+          <label for="edit-textarea">Textarea</label>
+          <textarea id="edit-textarea" name="textarea" rows="5" cols="60"
+                    aria-describedby="edit-textarea--description"></textarea>
+          <div id="edit-textarea--description" class="description">Textarea description goes here.</div>
+        </div>
+
+        <fieldset>
+          <legend>
+            <span class="fieldset-legend">Select any additional subjects that you would like to receive information about.</span>
+          </legend>
+          <div>
+            <div class="form-type-checkbox form-type-checkbox--side form-item">
+              <input id="financial-aid-2" type="checkbox" value="FINANCIAL_AID">
+              <label for="financial-aid-2" class="checkbox">Costs & Financial Aid</label>
+            </div>
+            <div class="form-type-checkbox form-type-checkbox--side form-item">
+              <input id="marching-band-2" type="checkbox" value="MARCHING_BAND">
+              <label for="marching-band-2" class="checkbox">Hawkeye Marching Band</label>
+            </div>
+            <div class="form-type-checkbox form-type-checkbox--side form-item">
+              <input id="rotc-air-force-2" type="checkbox" value="ROTC_AIR_FORCE">
+              <label for="rotc-air-force-2" class="checkbox">ROTC - Air Force (Aerospace Studies)</label>
+            </div>
+            <div class="form-type-checkbox form-type-checkbox--side form-item">
+              <input id="rotc-army-2" type="checkbox" value="ROTC_ARMY">
+              <label for="rotc-army-2" class="checkbox">ROTC - Army (Military Science)</label>
+            </div>
+            <div class="form-type-checkbox form-type-checkbox--side form-item">
+              <input id="study-abroad-2" type="checkbox" value="STUDY_ABROAD">
+              <label for="study-abroad-2" class="checkbox">Study Abroad</label>
+            </div>
+            <div class="form-type-checkbox form-type-checkbox--side form-item">
+              <input id="band-2" type="checkbox" value="BAND">
+              <label for="band-2" class="checkbox">University Concert Bands and Jazz Bands</label>
+            </div>
+            <div class="form-type-checkbox form-type-checkbox--side form-item">
+              <input id="honors-2" type="checkbox" value="HONORS">
+              <label for="honors-2" class="checkbox">University Honors Program</label>
+            </div>
+            <div class="form-type-checkbox form-type-checkbox--side form-item">
+              <input id="veteran-services-2" type="checkbox" value="VETERAN_SERVICES">
+              <label for="veteran-services-2" class="checkbox">Veteran & Military Connected Student Services</label>
+            </div>
+          </div>
+        </fieldset>
       </div>
     </form>
-  `,
+  `
 });
 export const MiscElements = MiscTemplate.bind({});
 MiscElements.args = {};
@@ -294,7 +313,7 @@ const FlexboxLayoutTemplate = (args) => ({
         </div>
       </div>
     </form>
-  `,
+  `
 });
 export const FlexboxLayout = FlexboxLayoutTemplate.bind({});
 FlexboxLayout.args = {};
@@ -356,7 +375,8 @@ const GridLayoutTemplate = (args) => ({
           </div>
         </div>
 
-        <div class="grid--threecol--33-34-33 grid--gap__sm bg--gray element--padding__all--minimal element--margin__bottom--extra">
+        <div
+          class="grid--threecol--33-34-33 grid--gap__sm bg--gray element--padding__all--minimal element--margin__bottom--extra">
           <div class="list-container__inner">
             <div class="form-item">
               <label for="first-name-grid4">First Name</label>
@@ -376,7 +396,7 @@ const GridLayoutTemplate = (args) => ({
         </div>
       </div>
     </form>
-  `,
+  `
 });
 export const GridLayout = GridLayoutTemplate.bind({});
 GridLayout.args = {};
@@ -386,7 +406,8 @@ const CompleteFormTemplate = (args) => ({
   setup() {
     return { args };
   },
-  template: `<uids-form />`,
+  template: `
+    <uids-form />`
 });
 
 
