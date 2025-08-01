4bea3daf51837cd2c3d9178758f9103b72b43d40
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2025-08-01 13:19:19 -0500

    Adding in more elements of layout and form.

--- src/components/showcase/ApplicationFormLayout.stories.js
+++ src/components/showcase/ApplicationFormLayout.stories.js
@@ -1,6 +1,6 @@
-// import '../../scss/components/form.scss';
+import '../../scss/components/form.scss';
 import '../../scss/layout/_grid.scss';
-// import '../../scss/layout/_flexbox.scss';
+import '../../scss/layout/_flexbox.scss';
 import '../../scss/components/_background.scss';
 import '../../scss/components/_border.scss';
 import '../../scss/components/_headline.scss';
@@ -11,7 +11,7 @@ import BrandBar from '../brand-bar/BrandBar.vue';
 import BrandBarStories from '../brand-bar/BrandBar.stories.js';
 
 export default {
-  title: 'Showcase/Application form',
+  title: 'Showcase/Forms',
   tags: ['!autodocs'],
   parameters: {
     docs: {
@@ -33,59 +33,126 @@ const Template = {
         :narrow='true'
         :healthcare='false'
       >
+        <div class="menu-user">
+          <ul class="menu">
+            <li><a href="/">Sign out</a></li>
+          </ul>
+        </div>
         Profiles
       </brand-bar>
-      <div class='user-menu'>
-        <uids-button
-          :url=""
-          :color='tertiary'
-          :size='small'
-          :borderless='true'
-          :full='false'
-          :transparent='true'
-          :light_font='true'
-        ><i style='color: var(--brand-secondary)' class='fas fa-house'></i>  Home</uids-button>
-        <uids-button
-          :url=""
-          :color='tertiary'
-          :size='small'
-          :borderless='true'
-          :full='false'
-          :transparent='true'
-          :light_font='true'
-        ><i style='color: var(--brand-secondary)' class='fas fa-people-line'></i>  Directories</uids-button>
-        <uids-button
-          :url=""
-          :color='tertiary'
-          :size='small'
-          :borderless='true'
-          :full='false'
-          :transparent='true'
-          :light_font='true'
-        ><i style='color: var(--brand-secondary)' class='fas fa-file'></i>  Reports</uids-button>
-        <uids-button
-          :url=""
-          :color='tertiary'
-          :size='small'
-          :borderless='true'
-          :full='false'
-          :transparent='true'
-          :light_font='true'
-        ><i style='color: var(--brand-secondary)' class='fas fa-user-gear'></i>  Roles</uids-button>
-        <uids-button
-          :url=""
-          :color='tertiary'
-          :size='small'
-          :borderless='true'
-          :full='false'
-          :transparent='true'
-          :light_font='true'
-        ><i style='color: var(--brand-secondary)' class='fas fa-mobile-screen'></i>  Contact</uids-button>
+      <div class="menu">
+        <ul class="flex">
+          <li>
+            <uids-button
+              :url=""
+              :color='tertiary'
+              :size='small'
+              :borderless='true'
+              :full='false'
+              :transparent='true'
+              :light_font='true'
+            ><i style='color: var(--brand-secondary)' class="fas fa-house"></i>  Home</uids-button>
+          </li>
+          <li>
+            <uids-button
+              :url=""
+              :color='tertiary'
+              :size='small'
+              :borderless='true'
+              :full='false'
+              :transparent='true'
+              :light_font='true'
+            ><i style="color: var(--brand-secondary)" class="fas fa-people-line"></i>  Directories</uids-button>
+          </li>
+          <li>
+            <uids-button
+              :url=""
+              :color='tertiary'
+              :size='small'
+              :borderless='true'
+              :full='false'
+              :transparent='true'
+              :light_font='true'
+            ><i style="color: var(--brand-secondary)" class="fas fa-file"></i>  Reports</uids-button>
+          </li>
+          <li>
+            <uids-button
+              :url=""
+              :color='tertiary'
+              :size='small'
+              :borderless='true'
+              :full='false'
+              :transparent='true'
+              :light_font='true'
+            ><i style="color: var(--brand-secondary)" class="fas fa-user-gear"></i>  Roles</uids-button>
+          </li>
+          <li>
+            <uids-button
+              :url=""
+              :color='tertiary'
+              :size='small'
+              :borderless='true'
+              :full='false'
+              :transparent='true'
+              :light_font='true'
+            ><i style='color: var(--brand-secondary)' class='fas fa-mobile-screen'></i>  Contact</uids-button>
+          </li>
+        </ul>
+      </div>
+      <div class="content flex flex--wrap flex--gap__sm">
+        <div class="menu menu-sidebar flex--1">
+          Sidebar menu here.
+        </div>
+        <div class="main-panel flex--5">
+          <div class="breadcrumb">
+            Directory
+          </div>
+          <h1 class="title">Francois Abboud</h1>
+          <h2 class="section">Introduction</h2>
+          <form class="uids-form element--margin__top--extra">
+            <div class="flex flex--wrap flex--gap__sm">
+              <div class="form-item flex--3">
+                <label for="first">First</label>
+                <input type="text" id="first" placeholder="First">
+              </div>
+              <div class="form-item flex--1">
+                <label for="middle">Middle</label>
+                <input type="text" id="middle" placeholder="Middle">
+              </div>
+              <div class="form-item flex--3">
+                <label for="last">Last</label>
+                <input type="text" id="last" placeholder="Last">
+              </div>
+              <div class="form-item flex--1">
+                <label for="suffix">Middle</label>
+                <input type="text" id="suffix" placeholder="Jr, Sr, III">
+              </div>
+            </div>
+            <div class="flex flex--wrap flex--gap__sm">
+              <div class="form-item flex--1">
+                <label for="middle">Credentials</label>
+                <input type="text" id="middle" placeholder="BS, MS, PhD">
+                <div class="form-help">No periods in degrees – PhD, not Ph.D.</div>
+              </div>
+              <div class="form-item flex--1">
+                <label for="email">Email</label>
+                <input type="text" id="email" placeholder="first-last@uiowa.edu">
+                <div class="form-help">UI email address</div>
+              </div>
+            </div>
+            <div class="form-item">
+              <label for="edit-textarea">Textarea</label>
+              <textarea id="edit-textarea" name="textarea" rows="5" cols="60"
+                        aria-describedby="edit-textarea--description"></textarea>
+              <div id="edit-textarea--description" class="description">Textarea description goes here.</div>
+            </div>
+          </form>
+        </div>
       </div>
     `,
   }),
 }
 
-export const InformationRequestForm = {
+export const ApplicationForm = {
   ...Template,
 }
