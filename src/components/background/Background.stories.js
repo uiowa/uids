dc1a4949eee3c77446cc8a8eb9410df39505c83d
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2025-12-12 09:41:57 -0600

    Another reference to the stub component removed.

--- src/components/background/Background.stories.js
+++ src/components/background/Background.stories.js
@@ -1,10 +1,8 @@
-import UidsStub from "../../components/stub/Stub.vue";
 import '../../scss/components/_background.scss'
 
 export default {
   title: 'Components/Background',
   tags: ['!autodocs'],
-  // https://github.com/storybookjs/storybook/issues/14442#issuecomment-1089165153
   parameters: {
     docs: {
       source: {
@@ -67,13 +65,9 @@ export default {
 };
 
 const Template = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
-  components: { UidsStub },
-  // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return { args };
   },
-  // And then the `args` are bound to your component with `v-bind="args"`
   template: `
     <div :class="args.section_background" style="min-height: 300px;">
     </div>
