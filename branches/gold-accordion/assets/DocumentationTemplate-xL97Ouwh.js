0fee0b15f8f5caf0cc1dc566e7878771b00fec29
pyrello <pyrello@users.noreply.github.com>
2026-01-14 15:34:38 +0000

    Deploying to gh-pages from @ uiowa/uids@bc7248451747a1533a311de0cd9ab1df25178e23 🚀

--- branches/gold-accordion/assets/DocumentationTemplate-xL97Ouwh.js
+++ branches/gold-accordion/assets/DocumentationTemplate-xL97Ouwh.js
@@ -1,4 +1,4 @@
-import{ae as n,af as e,ag as a,ah as r,ai as p,ak as m}from"./index-B61P74IW.js";import{u as i}from"./index-BbYYcJVu.js";import"./iframe-CxeL1x1e.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";function s(o){const t={h2:"h2",hr:"hr",p:"p",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{isTemplate:!0}),`
+import{ae as n,af as e,ag as a,ah as r,ai as p,ak as m}from"./index-DPSQ5dYt.js";import{u as i}from"./index-Bw7twwJQ.js";import"./iframe-MiXtmbd-.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";function s(o){const t={h2:"h2",hr:"hr",p:"p",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{isTemplate:!0}),`
 `,n.jsx(a,{}),`
 `,n.jsx(r,{}),`
 `,n.jsx(t.h2,{id:"inputs",children:"Inputs"}),`
---
+++ branches/gold-accordion/assets/index-DwjJ-mma.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-Bw7twwJQ.js";import"./index-DPSQ5dYt.js";import"./iframe-MiXtmbd-.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
