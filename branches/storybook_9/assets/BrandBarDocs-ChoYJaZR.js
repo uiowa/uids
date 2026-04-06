636067fb8500a3341b83584cecabe8665ebd6533
joewhitsitt <joewhitsitt@users.noreply.github.com>
2026-04-06 17:20:06 +0000

    Deploying to gh-pages from @ uiowa/uids@d0a89def2dcac65cb9d93b6a0618d267aad060eb 🚀

---
+++ branches/storybook_9/assets/BrandBarDocs-ChoYJaZR.js
@@ -0,0 +1,22 @@
+import{u as r,j as n,M as o,T as a,P as c,C as d,a as h}from"./iframe-CdHOF2jD.js";import{v as t}from"./package-CvWI4E4G.js";import"./preload-helper-BfYMYwp6.js";function i(e){const s={a:"a",h2:"h2",hr:"hr",li:"li",p:"p",strong:"strong",ul:"ul",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(o,{isTemplate:!0}),`
+`,n.jsx(a,{}),`
+`,n.jsx(s.h2,{id:"usage",children:"Usage"}),`
+`,n.jsxs(s.p,{children:["Make sure you are familiar with the ",n.jsx(s.a,{href:"../?path=/docs/introduction#getting-started",children:n.jsx(s.strong,{children:"Getting Started"})})," guide first."]}),`
+`,n.jsx(s.p,{children:"Add the following files to your project:"}),`
+`,n.jsxs(s.ul,{children:[`
+`,n.jsxs(s.li,{children:[`
+`,n.jsx("strong",{children:n.jsx("a",{href:`https://github.com/uiowa/uids/tree/gh-pages/docs/v${t}/dist/css/components/logo.css`,children:"logo.css"})}),`
+`]}),`
+`,n.jsxs(s.li,{children:[`
+`,n.jsx("strong",{children:n.jsx("a",{href:`https://github.com/uiowa/uids/tree/gh-pages/docs/v${t}/dist/css/components/brand-bar.css`,children:"brand-bar.css"})}),`
+`]}),`
+`]}),`
+`,n.jsx(s.p,{children:"Refer to the HTML tab for example HTML."}),`
+`,n.jsx(c,{withSource:"none"}),`
+`,n.jsx(s.h2,{id:"inputs",children:"Inputs"}),`
+`,n.jsx(s.p,{children:"The component accepts the following inputs (props):"}),`
+`,n.jsx(d,{}),`
+`,n.jsx(s.hr,{}),`
+`,n.jsx(s.h2,{id:"additional-variations",children:"Additional variations"}),`
+`,n.jsx(s.p,{children:"Listed below are additional variations of the component."}),`
+`,n.jsx(h,{})]})}function j(e={}){const{wrapper:s}={...r(),...e.components};return s?n.jsx(s,{...e,children:n.jsx(i,{...e})}):i(e)}export{j as default};
