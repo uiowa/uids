34d1efce0e1b840b64e1336fc768fe900085e721
joewhitsitt <joe-whitsitt@uiowa.edu>
2026-04-30 21:23:42 +0000

    Deploying to gh-pages from @ uiowa/uids@5b59c22a1ed8574f19a3cce69d617528fad55178 🚀

---
+++ latest/assets/formatter-EIJCOSYU-BTJzpJKd.js
@@ -0,0 +1,1 @@
+import{y as i,z as m,A as t}from"./iframe-BTbdoJxi.js";import"./preload-helper-CmP-MQqO.js";var a=i(m(),1),p=(0,a.default)(2)(async(e,r)=>e===!1?r:t(r));export{p as formatter};
---
+++ latest/assets/package-CidrV1oG.js
@@ -0,0 +1,1 @@
+const o="4.0.0";export{o as v};
