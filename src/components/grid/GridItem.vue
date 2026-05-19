84533e9bdbd2eba43aa643500a5eabc20b7fbbfc
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2026-05-19 14:21:15 -0500

    Start 5.x with a clean tree (#1049)

--- src/components/grid/GridItem.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="grid__column">
-    <slot></slot>
-  </div>
-</template>
-
-<script setup lang="ts">
-import '../../scss/layout/_grid.scss'
-</script>
--- src/components/utlity.ts
+++
@@ -1,5 +0,0 @@
-export function className(text: string): string {
-  return text.replace(/_/g, '-');
-}
-
-export  const V3_BASE_URL = 'https://uids.brand.uiowa.edu/3.x/';
