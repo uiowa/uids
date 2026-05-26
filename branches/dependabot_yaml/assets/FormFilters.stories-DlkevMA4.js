ff91d3c5c70d0d0cfc79ca5ecfdb45225192e3e7
joewhitsitt <joewhitsitt@users.noreply.github.com>
2026-05-26 20:23:14 +0000

    Deploying to gh-pages from @ uiowa/uids@d3193cc9905f88e33190ef626f123a596ffcf9a4 🚀

---
+++ branches/dependabot_yaml/assets/FormFilters.stories-DlkevMA4.js
@@ -0,0 +1,9 @@
+import{_ as s}from"./ExampleFiltersForm.vue_vue_type_script_setup_true_lang-C2gKBY4o.js";import"./iframe-CEK89M90.js";import"./preload-helper-C6G9HS8-.js";import"./FormFieldset.vue_vue_type_script_setup_true_lang-DKovtP_a.js";/* empty css             *//* empty css               */const u={components:{ExampleFiltersForm:s},title:"Components/Form",tags:["!autodocs"],parameters:{docs:{source:{code:null}}},argTypes:{}},m={render:a=>({components:{ExampleFiltersForm:s},setup(){return{args:a}},template:`
+      <div class="layout-container">
+        <example-filters-form
+        ></example-filters-form>
+      </div>
+    `})},e={...m,args:{}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  ...Template,
+  args: {}
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const F=["Filters"];export{e as Filters,F as __namedExportsOrder,u as default};
---
+++ branches/dependabot_yaml/assets/FormNumber.stories-Z0zoxMzk.js
@@ -0,0 +1,47 @@
+/* empty css             */import{_ as d}from"./FormContainer.vue_vue_type_script_setup_true_lang-DgLXsoaf.js";import"./iframe-CEK89M90.js";import"./preload-helper-C6G9HS8-.js";const b={title:"Elements/Form",parameters:{docs:{source:{code:null}}},tags:["!autodocs"],argTypes:{compact:{name:"Compact",control:{type:"boolean"},table:{category:"Modifiers"}},large:{name:"Large",control:{type:"boolean"},table:{category:"Modifiers"}},inline:{name:"Inline",control:{type:"boolean"},table:{category:"Modifiers"}},disabled:{name:"Disabled",control:{type:"boolean"},table:{category:"States"}},error:{name:"Error",control:{type:"boolean"},table:{category:"States"}},required:{name:"Required",control:{type:"boolean"},table:{category:"States"}},type:{table:{disable:!0}},name:{table:{disable:!0}},id:{table:{disable:!0}},label:{table:{disable:!0}},description:{table:{disable:!0}},min:{table:{disable:!0}},max:{table:{disable:!0}}},render:i=>({setup(){return{args:i}},components:{UidsFormContainer:d},template:`
+      <div class="layout-container">
+        <form class="form">
+          <uids-form-container
+            :compact="args.compact"
+            :large="args.large"
+            :inline="args.inline"
+          >
+            <div class="form-item">
+              <label :for="args.id" :class="args.required ? 'form--required' : false" v-html="args.label"></label>
+              <input :type="args.type" :name="args.id" :id="args.id" :class="{ 'form--error': args.error }" :disabled="args.disabled" :required="args.required" :min="args.min" step="1" :max="args.max">
+              <div class="form-item__description" v-if="args.description" v-html="args.description"></div>
+            </div>
+          </uids-form-container>
+        </form>
+      </div>
+    `})},e={args:{disabled:!1,error:!1,required:!1,compact:!1,large:!1,inline:!1,type:"number",id:"hours",label:"Credit hours completed",description:"Enter the number of credit hours you have completed.",min:0,max:1e4}},r={tags:["!dev"],args:{disabled:!1,error:!1,required:!1,compact:!1,large:!1,inline:!1,type:"range",id:"volume",label:"Volume",min:0,max:100}};var a,s,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    disabled: false,
+    error: false,
+    required: false,
+    compact: false,
+    large: false,
+    inline: false,
+    type: 'number',
+    id: 'hours',
+    label: 'Credit hours completed',
+    description: 'Enter the number of credit hours you have completed.',
+    min: 0,
+    max: 10000
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var l,t,n;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  tags: ['!dev'],
+  args: {
+    disabled: false,
+    error: false,
+    required: false,
+    compact: false,
+    large: false,
+    inline: false,
+    type: 'range',
+    id: 'volume',
+    label: 'Volume',
+    min: 0,
+    max: 100
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const f=["Number","Range"];export{e as Number,r as Range,f as __namedExportsOrder,b as default};
