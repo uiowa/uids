4278ab6f263cd59ef3cb04abbdf2593e98acff8a
pyrello <pyrello@users.noreply.github.com>
2026-09-10 18:54:01 +0000

    Deploying to gh-pages from @ uiowa/uids@1a145ae8af8260e9b6538e864d4fba42d6c94618 🚀

---
+++ branches/tokens_4x_primitives_semantic/assets/formatter-EIJCOSYU-Pi_yRhpt.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-fGSbh7xu.js";import{Et as t,G as n,K as r,Tt as i,U as a,W as o}from"./iframe-BnMcGYqF.js";var s;e((()=>{n(),o(),t(),s=(0,i(r(),1).default)(2)(async(e,t)=>e===!1?t:a(t))}))();export{s as formatter};
