84951428e9201f1726245faa4595ce83d1f16556
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2026-09-10 12:22:22 -0500

    Cleaned up web page showcase story.

--- src/components/showcase/WebPage.stories.js
+++ src/components/showcase/WebPage.stories.js
@@ -34,7 +34,7 @@ const programs = [
 ];
 
 export default {
-  title: 'Showcase/Landing page',
+  title: 'Showcase/Web page',
   tags: ['!autodocs'],
   parameters: {
     layout: 'fullscreen',
@@ -47,19 +47,19 @@ export default {
   argTypes: {
     intro_background: {
       ...BackgroundStories.argTypes.section_background,
-      name: 'Intro section',
+      name: 'Intro section background',
     },
     programs_background: {
       ...BackgroundStories.argTypes.section_background,
-      name: 'Programs section',
+      name: 'Program section background',
     },
-    panel_background: {
+    card_background: {
       ...BackgroundStories.argTypes.section_background,
-      name: 'Nested panel',
+      name: 'Card background',
     },
     stats_background: {
       ...BackgroundStories.argTypes.section_background,
-      name: 'Stats section',
+      name: 'Stats section background',
     },
   },
 };
@@ -103,7 +103,7 @@ const Template = {
         </uids-banner>
 
         <div :class="args.intro_background">
-          <div class="layout-container element--margin__top--extra element--margin__bottom--extra">
+          <div class="page__container element--padding__top">
             <uids-grid :type="'twocol--67-33'">
               <uids-grid-item>
                 <h2>Section heading</h2>
@@ -116,7 +116,7 @@ const Template = {
                 </p>
               </uids-grid-item>
               <uids-grid-item>
-                <div class="border element--margin__top">
+                <div class="element--margin__top">
                   <h3>Aside heading</h3>
                   <ul>
                     <li><a href="#">List link one</a></li>
@@ -128,54 +128,48 @@ const Template = {
             </uids-grid>
           </div>
         </div>
-
         <div :class="args.programs_background">
-          <div class="layout-container element--margin__top--extra element--margin__bottom--extra">
+          <div class="page__container element--padding__bottom element--padding__top">
             <h2>Section heading</h2>
             <uids-grid>
               <uids-grid-item v-for="p in programs" :key="p.title">
-                <uids-card :url="'#'" :link_text="'Read more'">
+                <uids-card :url="'#'" :link_text="'Read more'" :background="args.card_background">
                   <template #title>{{ p.title }}</template>
                   <div>{{ p.body }}</div>
                 </uids-card>
               </uids-grid-item>
             </uids-grid>
 
-            <div :class="args.panel_background" class="border element--margin__top--extra">
-              <div class="layout-container">
-                <h3>Panel heading</h3>
-                <uids-blockquote
-                  :content="'Pull quote inside a nested panel, to show the blockquote treatment.'"
-                  :footer="'Attribution line'"
-                />
+            <uids-blockquote
+              :content="'Pull quote inside a nested panel, to show the blockquote treatment.'"
+              :footer="'Attribution line'"
+            />
 
-                <h4>Table heading</h4>
-                <uids-table
-                  summary="Example table showing header, cell and link treatments"
-                  caption="Table caption"
-                  :highlight="true"
-                >
-                  <template #thead>
-                    <tr>
-                      <th scope="col">Header</th>
-                      <th scope="col">Header</th>
-                      <th scope="col">Header</th>
-                    </tr>
-                  </template>
-                  <template #tbody>
-                    <tr><th scope="row">Row label</th><td>Cell</td><td><a href="#">Cell link</a></td></tr>
-                    <tr><th scope="row">Row label</th><td>Cell</td><td><a href="#">Cell link</a></td></tr>
-                  </template>
-                </uids-table>
-                <p class="is-small">Small text, for notes under a table.</p>
-              </div>
-            </div>
+            <h4>Table heading</h4>
+            <uids-table
+              summary="Example table showing header, cell and link treatments"
+              caption="Table caption"
+              :highlight="true"
+            >
+              <template #thead>
+                <tr>
+                  <th scope="col">Header</th>
+                  <th scope="col">Header</th>
+                  <th scope="col">Header</th>
+                </tr>
+              </template>
+              <template #tbody>
+                <tr><th scope="row">Row label</th><td>Cell</td><td><a href="#">Cell link</a></td></tr>
+                <tr><th scope="row">Row label</th><td>Cell</td><td><a href="#">Cell link</a></td></tr>
+              </template>
+            </uids-table>
+            <p class="is-small">Small text, for notes under a table.</p>
+
           </div>
         </div>
 
         <div :class="args.stats_background">
-          <div class="layout-container element--margin__top--extra element--margin__bottom--extra">
-            <h2>Section heading on a pattern background</h2>
+          <div class="page__container element--padding__top element--padding__bottom">
             <uids-grid>
               <uids-grid-item>
                 <uids-stat :stat_title="'17,000'" :stat_summary="'Stat summary line'" />
@@ -187,10 +181,6 @@ const Template = {
                 <uids-stat :stat_title="'18'" :stat_suffix="':1'" :stat_summary="'Stat summary line'" />
               </uids-grid-item>
             </uids-grid>
-            <p class="is-large element--margin__top">
-              Large text, one step up from body copy.
-            </p>
-            <uids-button :url="'#'">Button</uids-button>
           </div>
         </div>
 
@@ -215,8 +205,8 @@ export const LandingPage = {
   ...Template,
   args: {
     intro_background: '',
-    programs_background: 'bg--gray',
-    panel_background: 'bg--white',
+    programs_background: 'bg--gold',
+    card_background: 'bg-gray',
     stats_background: 'bg--black--pattern--particle',
   },
 };
